326cb8f582654331cd0cba4e420566ce77c4cb16
Yota Toyama <raviqqe@gmail.com>
2017-03-26 23:54:40 +0900

    Add apply, plan, and destroy tasks

--- rakefile.rb
+++ rakefile.rb
@@ -1,4 +1,14 @@
-task :deploy
+task :apply do
+  sh 'terraform apply'
+end
+
+task :plan do
+  sh 'terraform plan'
+end
+
+task :destroy do
+  sh 'terraform destroy -force'
+end
 
 task :format do
   sh 'rubocop -a'
