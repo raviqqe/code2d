0f6f297e7faf711cc7a27187e458d1d27ac58f46
Yota Toyama <raviqqe@gmail.com>
2017-03-30 19:55:55 +0900

    Remove style things

--- src/client/Main.elm
+++ src/client/Main.elm
@@ -2,9 +2,7 @@ module Main exposing (..)
 
 import Html exposing (..)
 import Html.Events exposing (onClick)
-import Html.Attributes
 import Time exposing (..)
-import Css exposing (..)
 
 
 -- Model
@@ -30,61 +28,22 @@ type Msg
 -- View
 
 
-styles : List Css.Mixin -> Attribute Msg
-styles =
-    Css.asPairs >> Html.Attributes.style
-
-
-buttonStyle : Attribute Msg
-buttonStyle =
-    styles [ height (Css.em 3), width (Css.em 12) ]
-
-
 view : Model -> Html Msg
 view model =
     div
-        [ styles
-            [ height (vh 100)
-            , width (vw 100)
-            , backgroundColor (rgb 128 128 256)
-            ]
-        ]
+        []
         (case model.page of
             Menu ->
                 [ div
-                    [ styles
-                        [ position relative
-                        , top (pct 45)
-                        , width (pct 50)
-                        , height (pct 40)
-                        , margin auto
-                        , justifyContent spaceBetween
-                        , displayFlex
-                        ]
-                    ]
-                    [ button
-                        [ onClick Reset, buttonStyle ]
-                        [ Html.text "reset" ]
-                    , button
-                        [ onClick Resume, buttonStyle ]
-                        [ Html.text "resume" ]
+                    []
+                    [ button [ onClick Reset ] [ Html.text "reset" ]
+                    , button [ onClick Resume ] [ Html.text "resume" ]
                     ]
                 ]
 
             Timer ->
                 [ div
-                    [ onClick Pause
-                    , styles
-                        [ position relative
-                        , top (pct 45)
-                        , width (Css.em 6)
-                        , height (Css.em 3)
-                        , margin auto
-                        , textAlign center
-                        , color (rgb 255 255 255)
-                        , fontSize (px 80)
-                        ]
-                    ]
+                    [ onClick Pause ]
                     [ Html.text (toString model.time) ]
                 ]
         )
