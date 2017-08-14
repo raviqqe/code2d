1310a63039e81d46dcde0e8286f7ae34fc6b8ec5
Yota Toyama <raviqqe@gmail.com>
2017-08-14 22:22:01 +0900

    Run npm install before build

--- app/rakefile.rb
+++ app/rakefile.rb
@@ -1,11 +1,15 @@
-task :build do
+task :deps do
+  sh 'npm install'
+end
+
+task build: :deps do
   sh 'npx react-scripts-ts build'
 end
 
-task :test do
+task test: :deps do
   sh 'npx react-scripts-ts test --env=jsdom'
 end
 
-task :run do
+task run: :deps do
   sh 'npx react-scripts-ts start'
 end
