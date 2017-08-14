1f7e41c3a2f7bf6110001e9b10093b3329dd9208
Yota Toyama <raviqqe@gmail.com>
2017-08-14 12:00:24 +0900

    Serve index.html

--- main.tf
+++ main.tf
@@ -1,3 +1,20 @@
 provider "aws" {
   region = "us-east-1"
 }
+
+resource "aws_s3_bucket" "public" {
+  bucket = "code2d.net"
+  acl    = "public-read"
+
+  website = {
+    index_document = "index.html"
+  }
+}
+
+resource "aws_s3_bucket_object" "object" {
+  bucket       = "${aws_s3_bucket.public.id}"
+  key          = "index.html"
+  source       = "index.html"
+  content_type = "text/html"
+  acl          = "public-read"
+}
