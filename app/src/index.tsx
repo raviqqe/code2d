65eafb28717ec267651715d94b49e1cd962a87f7
Yota Toyama <raviqqe@gmail.com>
2017-08-14 21:58:30 +0900

    Remove code related to service worker

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,11 +1,7 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import './index.css';
+import * as React from "react";
+import * as ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root') as HTMLElement
-);
-registerServiceWorker();
+import App from "./App";
+import "./index.css";
+
+ReactDOM.render(<App />, document.getElementById("root") as HTMLElement);
