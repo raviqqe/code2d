82c18bb700df920b665736de2e62b6cc2e97d737
Yota Toyama <raviqqe@gmail.com>
2017-08-14 13:49:28 +0900

    Set up DNS zone

--- main.tf
+++ main.tf
@@ -1,9 +1,13 @@
+variable "domain" {
+  default = "code2d.net"
+}
+
 provider "aws" {
   region = "us-east-1"
 }
 
 resource "aws_s3_bucket" "public" {
-  bucket = "code2d.net"
+  bucket = "${var.domain}"
   acl    = "public-read"
 
   website = {
@@ -17,4 +21,20 @@ resource "aws_s3_bucket_object" "object" {
   source       = "index.html"
   content_type = "text/html"
   acl          = "public-read"
+}
+
+resource "aws_route53_zone" "main" {
+  name = "${var.domain}"
+}
+
+resource "aws_route53_record" "self" {
+  zone_id = "${aws_route53_zone.main.zone_id}"
+  name    = "${var.domain}"
+  type    = "A"
+
+  alias {
+    name                   = "${aws_s3_bucket.public.website_domain}"
+    zone_id                = "${aws_s3_bucket.public.hosted_zone_id}"
+    evaluate_target_health = true
+  }
 }
