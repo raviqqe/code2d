85238733e7cf2f731c1ba2ddb3a40dd25879d8dc
Yota Toyama <raviqqe@gmail.com>
2017-08-14 23:54:41 +0900

    Fix test scripts

--- app/src/App.test.tsx
+++ app/src/App.test.tsx
@@ -1,8 +1,7 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import App from './App';
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import App from "./App";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+it("renders without crashing", () => {
+    ReactDOM.render(<App />, document.createElement("div"));
 });
