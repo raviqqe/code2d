bcddef51f5056e6a6cef11065fdecdcb18f152ca
Yota Toyama <raviqqe@gmail.com>
2017-08-21 21:55:50 +0900

    Use CDN

--- main.tf
+++ main.tf
@@ -1,17 +1,58 @@
 variable "domain" {}
 variable "region" {}
 
+variable "origin_id" {
+  default = "public_bucket"
+}
+
 provider "aws" {
   region = "${var.region}"
 }
 
-resource "aws_s3_bucket" "public" {
+resource "aws_s3_bucket" "b" {
   bucket        = "${var.domain}"
-  acl           = "public-read"
+  acl           = "private"
   force_destroy = true
+}
 
-  website = {
-    index_document = "index.html"
+resource "aws_cloudfront_distribution" "d" {
+  origin {
+    domain_name = "${aws_s3_bucket.b.bucket_domain_name}"
+    origin_id   = "${var.origin_id}"
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = "index.html"
+
+  aliases = ["${var.domain}"]
+
+  default_cache_behavior {
+    allowed_methods        = ["GET", "HEAD"]
+    cached_methods         = ["GET", "HEAD"]
+    target_origin_id       = "${var.origin_id}"
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
   }
 }
 
@@ -25,8 +66,8 @@ resource "aws_route53_record" "self" {
   type    = "A"
 
   alias {
-    name                   = "${aws_s3_bucket.public.website_domain}"
-    zone_id                = "${aws_s3_bucket.public.hosted_zone_id}"
+    name                   = "${aws_cloudfront_distribution.d.domain_name}"
+    zone_id                = "${aws_cloudfront_distribution.d.hosted_zone_id}"
     evaluate_target_health = true
   }
 }
