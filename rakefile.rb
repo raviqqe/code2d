ba562b89fd8da5f91b7b40a2c43c5f91b90af437
Yota Toyama <raviqqe@gmail.com>
2017-08-22 00:56:30 +0900

    Update name servers on deployment

--- rakefile.rb
+++ rakefile.rb
@@ -17,6 +17,17 @@ task :deploy do
   cd 'app' do
     sh "BUCKET=#{DOMAIN} rake deploy"
   end
+
+  name_servers = `terraform output name_servers`
+                 .split(/[,\s]+/)
+                 .map { |s| 'Name=' + s }
+                 .join ' '
+
+  sh %W[
+    aws route53domains update-domain-nameservers
+    --domain #{DOMAIN}
+    --nameservers #{name_servers}
+  ].join ' '
 end
 
 task :withdraw do
