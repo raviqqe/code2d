e7b162dfe477f63957b80478351193e8abf473b7
Yota Toyama <raviqqe@gmail.com>
2017-08-22 14:07:51 +0900

    Clean up top page

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,22 +2,14 @@ import * as React from "react";
 
 import "./App.css";
 
-const logo = require("./logo.svg");
-
-class App extends React.Component {
+export default class extends React.Component {
     public render() {
         return (
-            <div className="App">
+            <div>
                 <div className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <h2>Welcome to React</h2>
+                    <h1>code2d</h1>
                 </div>
-                <p className="App-intro">
-                    To get started, edit <code>src/App.tsx</code> and save to reload.
-                </p>
             </div>
         );
     }
 }
-
-export default App;
