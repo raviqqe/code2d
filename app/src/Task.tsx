2f29babd5aa0a7287615268a1a8297cdb93f44cb
Yota Toyama <raviqqe@gmail.com>
2017-08-22 23:57:25 +0900

    Enable drag and drop in the same task list

--- app/src/Task.tsx
+++ app/src/Task.tsx
@@ -1,9 +1,39 @@
 import * as React from "react";
+import { DragSource, DropTarget } from "react-dnd";
 
 import "./Task.css";
 
-export default class extends React.Component {
+interface IProps {
+    id: string;
+    onDrop: (id: string, swappedId: string) => void;
+}
+
+@DragSource("Task", {
+    beginDrag: (props) => props,
+}, (connect, monitor) => ({
+    connectDragSource: connect.dragSource(),
+    isDragging: monitor.isDragging(),
+}))
+@DropTarget("Task", {
+    drop: (droppedTask, monitor) => {
+        const draggedTask = monitor.getItem();
+
+        if (droppedTask.id !== draggedTask.id) {
+            return droppedTask.onDrop(droppedTask, draggedTask);
+        }
+    },
+}, (connect, monitor) => ({
+    canDrop: monitor.canDrop(),
+    connectDropTarget: connect.dropTarget(),
+    isOver: monitor.isOver(),
+    isOverCurrent: monitor.isOver({ shallow: true }),
+    itemType: monitor.getItemType(),
+}))
+export default class extends React.Component<IProps> {
     public render() {
-        return <div className="Task">My task</div>;
+        const { connectDragSource, connectDropTarget } = this.props as any;
+
+        return connectDragSource(connectDropTarget(
+            <div className="Task">{"Task ID:" + this.props.id}</div>));
     }
 }
--- app/src/TaskList.tsx
+++ app/src/TaskList.tsx
@@ -1,15 +1,28 @@
+import * as _ from "lodash";
 import * as React from "react";
 
 import Task from "./Task";
 import "./TaskList.css";
 
 export default class extends React.Component {
+    public state = {
+        tasks: [{ id: "foo" }, { id: "bar" }, { id: "baz" }],
+    };
+
     public render() {
+        const swapTasks = (task, swappedTask) => {
+            const tasks = [...this.state.tasks];
+
+            tasks[_.findIndex(this.state.tasks, { id: task.id })] = swappedTask;
+            tasks[_.findIndex(this.state.tasks, { id: swappedTask.id })] = task;
+
+            this.setState({ tasks });
+        };
+
         return (
             <div className="TaskList">
-                <Task />
-                <Task />
-                <Task />
+                {this.state.tasks.map((task) =>
+                    <Task {...task} key={task.id} onDrop={swapTasks} />)}
             </div>
         );
     }
