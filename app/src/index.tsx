3168f72975dfd66207fa670ceda095bdceacb98e
Yota Toyama <raviqqe@gmail.com>
2017-08-23 23:53:57 +0900

    Integrate redux

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,9 +1,15 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 
 import App from "./App";
 import "./index.css";
+import createStore from "./redux";
 import registerServiceWorker from "./register-service-worker";
 
-ReactDOM.render(<App />, document.getElementById("root") as HTMLElement);
+const store = createStore();
+
+ReactDOM.render(
+    <Provider store={store}><App /></Provider>,
+    document.getElementById("root") as HTMLElement);
 registerServiceWorker();
