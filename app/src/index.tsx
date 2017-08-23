e2f82c1c229bd0776efa0cb82ceb0435f9cf06ec
Yota Toyama <raviqqe@gmail.com>
2017-08-23 21:30:37 +0900

    Rename service worker script

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -3,7 +3,7 @@ import * as ReactDOM from "react-dom";
 
 import App from "./App";
 import "./index.css";
-import registerServiceWorker from "./registerServiceWorker";
+import registerServiceWorker from "./register-service-worker";
 
 ReactDOM.render(<App />, document.getElementById("root") as HTMLElement);
 registerServiceWorker();
