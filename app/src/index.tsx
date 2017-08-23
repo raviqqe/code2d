3194dc65b05a36b0c97f8656a1f115d961d388e1
Yota Toyama <raviqqe@gmail.com>
2017-08-23 13:46:22 +0900

    Register service worker

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -3,5 +3,7 @@ import * as ReactDOM from "react-dom";
 
 import App from "./App";
 import "./index.css";
+import registerServiceWorker from "./registerServiceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root") as HTMLElement);
+registerServiceWorker();
