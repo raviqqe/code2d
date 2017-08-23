9b89b11ecc0b69976e267912718db63131e41f5f
Yota Toyama <raviqqe@gmail.com>
2017-08-23 23:28:08 +0900

    Add sign-in duck

---
+++ app/src/redux/sign-in.ts
@@ -0,0 +1,33 @@
+import { SagaIterator } from "redux-saga";
+import { call, takeEvery } from "redux-saga/effects";
+import actionCreatorFactory from "typescript-fsa";
+import { reducerWithInitialState } from "typescript-fsa-reducers";
+
+import * as aws from "../lib/aws";
+import { unwrapAction } from "./utils";
+
+export const signIn = actionCreatorFactory().async<
+    { token: string },
+    undefined,
+    { message: string }>("SIGN_IN");
+
+export const initialState = {
+    errorMessage: "",
+    halfway: false,
+};
+
+export const reducer = reducerWithInitialState(initialState)
+    .case(signIn.started, () => ({ errorMessage: "", halfway: true }))
+    .case(signIn.done, () => ({ errorMessage: "", halfway: false }))
+    .case(signIn.failed, (_, { error: { message } }) => ({
+        errorMessage: message,
+        halfway: false,
+    }));
+
+export const sagas = [
+    takeEvery(signIn.started.type, unwrapAction(
+        signIn.started,
+        function* _({ token }): SagaIterator {
+            yield call(aws.signIn, token);
+        })),
+];
