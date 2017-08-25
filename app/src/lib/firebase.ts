e31d70085c281a1739bf9002d0174cc06b59596c
Yota Toyama <raviqqe@gmail.com>
2017-08-25 22:11:00 +0900

    Add tasks DAO

--- app/src/lib/firebase.ts
+++ app/src/lib/firebase.ts
@@ -1,5 +1,7 @@
 import * as firebase from "firebase";
 
+import ITask from "./task";
+
 const projectId = process.env.REACT_APP_FIREBASE_PROJECT_ID;
 
 export function initialize(): void {
@@ -14,4 +16,18 @@ export function initialize(): void {
 export async function signIn(): Promise<void> {
     await firebase.auth().signInWithRedirect(new firebase.auth.GithubAuthProvider());
     await firebase.auth().getRedirectResult();
+}
+
+export class Tasks {
+    public create(task: ITask): string {
+        return this.ref.push(task).key;
+    }
+
+    public async findAll(): Promise<{ [key: string]: ITask }> {
+        return (await this.ref.once("value")).val();
+    }
+
+    private get ref(): firebase.database.Reference {
+        return firebase.database().ref(`users/${firebase.auth().currentUser.uid}/tasks`);
+    }
 }
