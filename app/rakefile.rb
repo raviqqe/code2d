7f51d13059a965c03f68d3fa0e7eefe50073989e
Yota Toyama <raviqqe@gmail.com>
2017-08-27 19:51:14 +0900

    Don't build app before deployment

--- app/rakefile.rb
+++ app/rakefile.rb
@@ -16,7 +16,7 @@ task run: :build do
   sh 'npx react-scripts-ts start'
 end
 
-task deploy: :build do
+task :deploy do
   sh "aws s3 cp --acl public-read --recursive build s3://#{ENV['BUCKET']}"
 end
 
