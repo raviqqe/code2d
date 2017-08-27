215b97ce206fb23345f38594ecd39942b23e52a5
Yota Toyama <raviqqe@gmail.com>
2017-08-27 23:07:49 +0900

    Fix truncateTasks functions

--- functions/index.ts
+++ functions/index.ts
@@ -1,17 +1,47 @@
 import * as functions from "firebase-functions";
+import * as _ from "lodash";
 
 const maxTasks = 64;
 
-export const limitTasks = functions.database.ref("users/{userId}/tasks/{state}/{taskId}").onCreate(
-    async ({ data: { ref: { parent } } }) => {
-        const snapshot = await parent.once("value");
+interface ITask {
+    key?: string;
+    updatedAt: number;
+}
 
-        if (snapshot.numChildren() >= maxTasks) {
-            const idToTask = snapshot.val();
-
-            await parent.update(Object.keys(idToTask)
-                .map((id: string) => idToTask[id])
-                .sort((x, y) => x.updatedAt - y.updatedAt)
-                .slice(0, maxTasks));
+export const truncateUndoneTasks = createTruncateTasksFunction(
+    "undone",
+    (tasks: ITask[]) => {
+        for (const task of extractOldTasks(tasks)) {
+            _.remove(tasks, task);
         }
+
+        return tasks;
     });
+
+export const truncateDoneTasks = createTruncateTasksFunction(
+    "done",
+    (keyToTask: { [key: string]: ITask }) => {
+        for (const { key } of extractOldTasks(
+            Object.keys(keyToTask).map((key) => ({ key, ...keyToTask[key] })))) {
+            delete keyToTask[key];
+        }
+
+        return keyToTask;
+    });
+
+function createTruncateTasksFunction<T>(
+    state: "done" | "undone",
+    callback: (tasks: T) => T) {
+    return functions.database.ref(`users/{userId}/tasks/${state}/{taskId}`).onCreate(
+        async ({ data: { ref: { parent } } }) => {
+            const snapshot = await parent.once("value");
+
+            if (snapshot.numChildren() >= maxTasks) {
+                await parent.set(callback(snapshot.val()));
+            }
+        });
+}
+
+function extractOldTasks(tasks: ITask[]): ITask[] {
+    return [...tasks].sort((x, y) => y.updatedAt - x.updatedAt).slice(maxTasks);
+}
