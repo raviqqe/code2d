90bad444911a0484b16144f31f07dbcd9f0bd944
Yota Toyama <raviqqe@gmail.com>
2017-08-28 14:03:08 +0900

    Use EditableText components

--- app/src/component/Task.tsx
+++ app/src/component/Task.tsx
@@ -4,6 +4,7 @@ import { connect } from "react-redux";
 
 import { ITask } from "../lib/task";
 import { actionCreators } from "../redux/tasks";
+import EditableText from "./EditableText";
 import "./style/Task.css";
 
 interface IProps extends ITask {
@@ -11,53 +12,13 @@ interface IProps extends ITask {
 }
 
 class Task extends React.Component<IProps> {
-    public state = {
-        description: "",
-        editingDescription: false,
-        editingName: false,
-        name: "",
-    };
-
     public render() {
         const { name, description, createdAt, updatedAt } = this.props;
-        const { editingName, editingDescription } = this.state;
 
         return (
             <div className="Task">
-                {editingName ? (
-                    <input
-                        type="text"
-                        value={this.state.name}
-                        onChange={({ target: { value } }) =>
-                            this.setState({ name: value })}
-                        onKeyPress={({ charCode }) => {
-                            if (charCode === 13) {
-                                this.setState({ editingName: false });
-                            }
-                        }}
-                    />
-                ) : (
-                        <div onClick={() => this.setState({ editingName: true, name })}>
-                            {name}
-                        </div>
-                    )}
-                {editingDescription ? (
-                    <input
-                        type="text"
-                        value={this.state.description}
-                        onChange={({ target: { value } }) =>
-                            this.setState({ description: value })}
-                        onKeyPress={({ charCode }) => {
-                            if (charCode === 13) {
-                                this.setState({ editingDescription: false });
-                            }
-                        }}
-                    />
-                ) : (
-                        <div onClick={() => this.setState({ editingDescription: true, description })}>
-                            {description}
-                        </div>
-                    )}
+                <EditableText text={name} onEdit={() => undefined} />
+                <EditableText text={description} onEdit={() => undefined} />
                 <div
                     onClick={() => this.props.markDoneTask({
                         createdAt,
