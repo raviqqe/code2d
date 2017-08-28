847c83cdd5686d29b5535d516943361a3883089d
Yota Toyama <raviqqe@gmail.com>
2017-08-28 15:35:17 +0900

    Fix focus events of EditableText

--- app/src/component/EditableText.tsx
+++ app/src/component/EditableText.tsx
@@ -16,11 +16,26 @@ export default class extends React.Component<IProps, IState> {
         text: "",
     };
 
+    private input: { focus: () => void, value: string };
+
+    public componentDidUpdate(_, { editing }: IState) {
+        if (!editing && this.state.editing) {
+            this.input.focus(); // Do this after rendering.
+
+            // Set an initial value and move a cursor to end.
+            this.input.value = "";
+            this.input.value = this.props.text;
+            this.setState({ text: this.props.text });
+        }
+    }
+
     public render() {
         return this.state.editing ? (
             <input
+                ref={(input) => { this.input = input; }}
                 type="text"
                 value={this.state.text}
+                onBlur={() => this.setState({ editing: false })}
                 onChange={({ target: { value } }) => this.setState({ text: value })}
                 onKeyPress={({ charCode }) => {
                     if (charCode === 13) {
@@ -30,12 +45,7 @@ export default class extends React.Component<IProps, IState> {
                 }}
             />
         ) : (
-                <div
-                    onClick={() => this.setState({
-                        editing: true,
-                        text: this.props.text,
-                    })}
-                >
+                <div onClick={() => this.setState({ editing: true })}>
                     {this.props.text}
                 </div>
             );
