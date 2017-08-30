28062066bf1105ff6667336229e4036694ea3069
Yota Toyama <raviqqe@gmail.com>
2017-08-30 13:27:26 +0900

    Simplify TaskName

--- app/src/component/TaskName.tsx
+++ app/src/component/TaskName.tsx
@@ -1,88 +1,43 @@
 import * as React from "react";
 import Markdown = require("react-markdown");
 
+import InputComponent, { IState } from "./InputComponent";
+
 interface IProps {
     className?: string;
     inputClassName?: string;
     editable?: boolean;
     onEdit: (text: string) => void;
     text: string;
-    textArea?: boolean;
 }
 
-interface IState {
-    editing: boolean;
-    text: string;
-}
-
-export default class extends React.Component<IProps, IState> {
-    public state: IState = {
-        editing: false,
-        text: "",
-    };
-
-    private input: { focus: () => void, value: string };
-
-    public startEditing = (): void => {
-        if (this.props.editable || this.props.editable === undefined) {
-            this.setState({ editing: true });
-        }
-    }
-
-    public stopEditing = (): void => {
-        this.setState({ editing: false });
-        this.props.onEdit(this.state.text);
-    }
-
-    public cancelEditing = (): void => this.setState({ editing: false });
-
-    public componentDidUpdate(_, { editing }: IState) {
-        if (!editing && this.state.editing) {
-            this.input.focus(); // Do this after rendering.
-
-            // Set an initial value and move a cursor to end.
-            this.input.value = "";
-            this.input.value = this.props.text;
-            this.setState({ text: this.props.text });
-        }
-    }
-
+export default class extends InputComponent<IProps> {
     public render() {
-        if (!this.state.editing && this.props.text.trim()) {
+        if (this.props.editable && this.state.editing) {
             return (
-                <div
-                    className={this.props.className}
-                    onClick={this.props.textArea ? undefined : this.startEditing}
-                >
-                    {this.props.textArea
-                        ? <Markdown className="EditableText-markdown" source={this.props.text} />
-                        : this.props.text}
-                </div>
-            );
-        } else if (!this.state.editing) {
-            return false;
-        }
-
-        const commonProps = {
-            className: this.props.inputClassName,
-            onChange: ({ target: { value } }) => this.setState({ text: value }),
-            ref: (input) => { this.input = input; },
-            value: this.state.text,
-        };
-
-        return this.props.textArea
-            ? <textarea {...commonProps} />
-            : (
                 <input
-                    {...commonProps}
-                    onBlur={this.cancelEditing}
+                    className={this.props.inputClassName}
+                    onBlur={() => this.setState({ editing: false })}
+                    onChange={({ target: { value } }) => this.setState({ text: value })}
                     onKeyPress={({ charCode }) => {
                         if (charCode === 13) {
-                            this.stopEditing();
+                            this.setState({ editing: false });
                             this.props.onEdit(this.state.text);
                         }
                     }}
+                    ref={(input) => { this.input = input; }}
+                    value={this.state.text}
                 />
             );
+        }
+
+        return (
+            <div
+                className={this.props.className}
+                onClick={() => this.setState({ editing: this.props.editable })}
+            >
+                {this.props.text}
+            </div>
+        );
     }
 }
