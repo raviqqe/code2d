6563b140de7423b377a450111f9fc536911999ac
Yota Toyama <raviqqe@gmail.com>
2017-08-30 15:39:34 +0900

    Add side bar to home page

--- app/src/container/Home.tsx
+++ app/src/container/Home.tsx
@@ -3,14 +3,22 @@ import { connect } from "react-redux";
 import { Redirect } from "react-router-dom";
 
 import DoneTasks from "../component/DoneTasks";
+import Task from "../component/Task";
 import TodoTasks from "../component/TodoTasks";
+import { ITask } from "../lib/task";
 import "./style/Home.css";
+
+interface IProps {
+    signedIn: boolean;
+    doneTasks: ITask[];
+    todoTasks: ITask[];
+}
 
 interface IState {
     showDoneTasks: boolean;
 }
 
-class Home extends React.Component<{ signedIn: boolean }, IState> {
+class Home extends React.Component<IProps, IState> {
     public state: IState = { showDoneTasks: false };
 
     public render() {
@@ -19,16 +27,23 @@ class Home extends React.Component<{ signedIn: boolean }, IState> {
         }
 
         const { showDoneTasks } = this.state;
+        const Tasks = showDoneTasks ? DoneTasks : TodoTasks;
+        const task = showDoneTasks ? this.props.doneTasks[0] : this.props.todoTasks[0];
 
         return (
             <div>
-                <div onClick={() => this.setState({ showDoneTasks: !showDoneTasks })}>
+                <button onClick={() => this.setState({ showDoneTasks: !showDoneTasks })}>
                     {showDoneTasks ? "done" : "todo"}
+                </button>
+                <div className="Home-container">
+                    <Tasks />
+                    <div className="Home-sidebar">
+                        {task && <Task {...task} />}
+                    </div>
                 </div>
-                {showDoneTasks ? <DoneTasks /> : <TodoTasks />}
             </div>
         );
     }
 }
 
-export default connect(({ authState }) => authState, {})(Home);
+export default connect(({ authState, tasks }) => ({ ...authState, ...tasks }), {})(Home);
