c9521b239ffc72b1c35140cf8768fe9a9d57c87e
Yota Toyama <raviqqe@gmail.com>
2017-09-03 15:46:20 +0900

    Remove pause button

--- app/src/container/Timer.tsx
+++ app/src/container/Timer.tsx
@@ -6,27 +6,18 @@ import { Link } from "react-router-dom";
 import { actionCreators } from "../redux/timer";
 import "./style/Timer.css";
 
-interface IProps {
-    toggleTimer: () => void;
-}
-
 interface IState {
-    paused: boolean;
     seconds: number;
 }
 
-class Timer extends React.Component<IProps, IState> {
-    public state: IState = { paused: false, seconds: 25 * 60 };
+class Timer extends React.Component<{ toggleTimer: () => void }, IState> {
+    public state: IState = { seconds: 25 * 60 };
 
     private timer;
 
     public componentDidMount() {
         this.timer = window.setInterval(
-            () => {
-                if (!this.state.paused) {
-                    this.setState({ seconds: Math.max(this.state.seconds - 1, 0) });
-                }
-            },
+            () => this.setState({ seconds: Math.max(this.state.seconds - 1, 0) }),
             1000);
     }
 
@@ -35,7 +26,7 @@ class Timer extends React.Component<IProps, IState> {
     }
 
     public render() {
-        const { paused, seconds } = this.state;
+        const { seconds } = this.state;
 
         return (
             <div className="Timer-container">
@@ -47,9 +38,6 @@ class Timer extends React.Component<IProps, IState> {
                         {numeral(seconds % 60).format("00")}
                     </div>
                 </div>
-                <button onClick={() => this.setState({ paused: !paused })}>
-                    {paused ? "restart" : "pause"}
-                </button>
                 <button className="Timer-button" onClick={this.props.toggleTimer}>
                     <Link to="/tasks/todo">cancel</Link>
                 </button>
