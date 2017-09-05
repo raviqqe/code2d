5e42a253807199e8f0d8af06b08aac53bb0bef15
Yota Toyama <raviqqe@gmail.com>
2017-09-05 22:17:11 +0900

    Set exact options on routes

--- app/src/container/App.tsx
+++ app/src/container/App.tsx
@@ -9,11 +9,11 @@ export default class extends React.Component {
         return (
             <BrowserRouter>
                 <div>
-                    <Route path="/" render={() => <Redirect to="/tasks" />} />
-                    <Route path="/tasks" render={() => <Redirect to="/tasks/todo" />} />
-                    <Route path="/tasks/todo" render={() => <Tasks />} />
-                    <Route path="/tasks/done" render={() => <Tasks {...{ done: true }} />} />
-                    <Route path="/sign-in" component={SignIn} />
+                    <Route exact={true} path="/" render={() => <Redirect to="/tasks" />} />
+                    <Route exact={true} path="/tasks" render={() => <Redirect to="/tasks/todo" />} />
+                    <Route exact={true} path="/tasks/todo" render={() => <Tasks />} />
+                    <Route exact={true} path="/tasks/done" render={() => <Tasks {...{ done: true }} />} />
+                    <Route exact={true} path="/sign-in" component={SignIn} />
                 </div>
             </BrowserRouter>
         );
