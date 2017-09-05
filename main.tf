8cb0360f97fde03b010f37a832067d76a79179d3
Yota Toyama <raviqqe@gmail.com>
2017-09-05 22:45:25 +0900

    Set TXT record for hosting

--- main.tf
+++ main.tf
@@ -64,6 +64,14 @@ resource "aws_route53_zone" "z" {
   name = "${var.domain}"
 }
 
+resource "aws_route53_record" "txt" {
+  zone_id = "${aws_route53_zone.z.zone_id}"
+  name    = "${var.domain}"
+  ttl     = 5
+  type    = "TXT"
+  records = ["google-site-verification=-pUVh2OF_SLYQTlifb-0WOG_Bzgh19ZVQwwxyRiLMVg"]
+}
+
 resource "aws_route53_record" "r" {
   zone_id = "${aws_route53_zone.z.zone_id}"
   name    = "${var.domain}"
