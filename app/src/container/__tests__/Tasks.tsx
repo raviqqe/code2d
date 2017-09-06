160c59f47fa1a43ec9e8e4f07e89371074f93127
Yota Toyama <raviqqe@gmail.com>
2017-09-06 13:54:13 +0900

    Test a done tasks page

--- app/src/container/__tests__/Tasks.tsx
+++ app/src/container/__tests__/Tasks.tsx
@@ -6,12 +6,25 @@ import { BrowserRouter, Route } from "react-router-dom";
 import createStore from "../../redux";
 import Tasks from "../Tasks";
 
-it("renders without crashing", () => {
+it("renders a todo tasks page", () => {
     ReactDOM.render(
         <Provider store={createStore()}>
             <BrowserRouter>
                 <div>
                     <Route exact={true} path="/" render={() => <Tasks />} />
+                    <Route exact={true} path="/sign-in" render={() => <div>sign-in page mock</div>} />
+                </div>
+            </BrowserRouter>
+        </Provider>,
+        document.createElement("div"));
+});
+
+it("renders a done tasks page", () => {
+    ReactDOM.render(
+        <Provider store={createStore()}>
+            <BrowserRouter>
+                <div>
+                    <Route exact={true} path="/" render={() => <Tasks {...{ done: true }} />} />
                     <Route exact={true} path="/sign-in" render={() => <div>sign-in page mock</div>} />
                 </div>
             </BrowserRouter>
