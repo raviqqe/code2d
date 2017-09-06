7902062426b191c34094b0b91df2baade4e68087
Yota Toyama <raviqqe@gmail.com>
2017-09-06 19:56:34 +0900

    Test sagas of sign-in duck

--- app/src/redux/__tests__/sign-in.ts
+++ app/src/redux/__tests__/sign-in.ts
@@ -1,6 +1,20 @@
+import createStore from "..";
+import { sleep } from "../../lib/utils";
 import { actionCreators, initialState, reducer } from "../sign-in";
 
 it("tries to sign in", () => {
     expect(initialState.halfway).toBe(false);
     expect(reducer(initialState, actionCreators.signIn()).halfway).toBe(true);
+});
+
+it("tries to sign in", async () => {
+    for (const mock of [{ signIn: () => undefined }, { signIn: () => { throw new Error(); } }]) {
+        jest.doMock("../../lib/firebase", () => mock);
+
+        const store = createStore();
+
+        store.dispatch(actionCreators.signIn());
+        await sleep(100);
+        expect((store.getState() as any).signIn.halfway).toBe(false);
+    }
 });
