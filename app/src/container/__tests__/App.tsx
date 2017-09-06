513a619849b2bdfcd2667289681a50c42058a273
Yota Toyama <raviqqe@gmail.com>
2017-09-06 11:37:03 +0900

    Fix web app test

--- app/src/container/__tests__/App.tsx
+++ app/src/container/__tests__/App.tsx
@@ -1,7 +1,15 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+
+import * as firebase from "../../lib/firebase";
+import createStore from "../../redux";
 import App from "../App";
 
+firebase.initialize();
+
 it("renders without crashing", () => {
-    ReactDOM.render(<App />, document.createElement("div"));
+    ReactDOM.render(
+        <Provider store={createStore()}><App /></Provider>,
+        document.createElement("div"));
 });
