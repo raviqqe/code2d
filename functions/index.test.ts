765b8536f7fdda880abe2e7363dd273a93fa1cc6
Yota Toyama <raviqqe@gmail.com>
2017-09-10 21:42:16 +0900

    Remove an old function

--- functions/index.test.ts
+++ functions/index.test.ts
@@ -1,12 +1,1 @@
-import { extractOldTasks } from "./index";
-
-jest.mock(
-    "firebase-functions",
-    () => ({ database: { ref: () => ({ onCreate: () => undefined }) } }));
-
-describe("extractOldTasks", () => {
-    it("extracts old tasks", () => {
-        const tasks = [{ updatedAt: 3 }, { updatedAt: 1 }, { updatedAt: 4 }, { updatedAt: 2 }];
-        expect(extractOldTasks(tasks, 2)).toEqual([{ updatedAt: 2 }, { updatedAt: 1 }]);
-    });
-});
+test("DUMMY TEST", () => undefined);
--- functions/index.ts
+++ functions/index.ts
@@ -1,27 +1,2 @@
+import * as admin from "firebase-admin";
 import * as functions from "firebase-functions";
-import * as _ from "lodash";
-
-const maxTasks = 64;
-
-interface ITask {
-    updatedAt: number;
-}
-
-export const truncateTasks = functions.database.ref("users/{userId}/tasks/{state}/{taskId}").onCreate(
-    async ({ data: { ref: { parent } } }) => {
-        const snapshot = await parent.once("value");
-
-        if (snapshot.numChildren() > maxTasks) {
-            const tasks: ITask[] = snapshot.val();
-
-            for (const task of extractOldTasks(tasks)) {
-                _.remove(tasks, task);
-            }
-
-            await parent.set(tasks);
-        }
-    });
-
-export function extractOldTasks(tasks: ITask[], localMaxTasks = maxTasks): ITask[] {
-    return _.sortBy(tasks, ({ updatedAt }) => -updatedAt).slice(localMaxTasks);
-}
