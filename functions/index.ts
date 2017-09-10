cc655d28d2fa20ba29ec8dd8dec5c93ec44403b0
Yota Toyama <raviqqe@gmail.com>
2017-09-10 21:48:19 +0900

    Initialize admin SDK

--- functions/index.ts
+++ functions/index.ts
@@ -2,6 +2,8 @@ import { Request, Response } from "express";
 import * as admin from "firebase-admin";
 import * as functions from "firebase-functions";
 
+admin.initializeApp(functions.config().firebase);
+
 export const books = functions.https.onRequest((request: Request, response: Response) => {
     // TODO: Fetch books.
 });
