740464fe88fd10c96f882e5fed3c2c78eec65967
Yota Toyama <raviqqe@gmail.com>
2017-09-10 22:08:29 +0900

    Initiazlie amazon product API client

--- functions/index.ts
+++ functions/index.ts
@@ -1,8 +1,16 @@
+import * as amazon from "amazon-product-api";
 import { Request, Response } from "express";
 import * as admin from "firebase-admin";
 import * as functions from "firebase-functions";
 
+const config = functions.config();
+
 admin.initializeApp(functions.config().firebase);
+const amazonClient = amazon.createClient({
+    awsId: config.aws.id,
+    awsSecret: config.aws.secret,
+    awsTag: config.aws.tag,
+});
 
 export const books = functions.https.onRequest(async (request: Request, response: Response) => {
     await admin.auth().verifyIdToken(request.query.token);
