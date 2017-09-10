2c50484de51aadcd3f01462b71450bb43ed5d9a1
Yota Toyama <raviqqe@gmail.com>
2017-09-10 21:46:26 +0900

    Add dummy function of books

--- functions/index.ts
+++ functions/index.ts
@@ -1,2 +1,7 @@
+import { Request, Response } from "express";
 import * as admin from "firebase-admin";
 import * as functions from "firebase-functions";
+
+export const books = functions.https.onRequest((request: Request, response: Response) => {
+    // TODO: Fetch books.
+});
