3485744148155b1be3d63897de35344fedf5d38f
Yota Toyama <raviqqe@gmail.com>
2017-09-11 21:47:01 +0900

    Get books from CDN

--- app/src/lib/functions.ts
+++ app/src/lib/functions.ts
@@ -2,13 +2,12 @@ import axios from "axios";
 import * as firebase from "firebase";
 import * as url from "url";
 
-const hostname = (
-    `${process.env.REACT_APP_FIREBASE_REGION}-` +
-    `${process.env.REACT_APP_FIREBASE_PROJECT_ID}` +
-    ".cloudfunctions.net");
-
 export async function call<R = any>(pathname: string): Promise<R> {
     return (await axios.get(
-        url.format({ hostname, pathname: "/" + pathname, protocol: "https" }),
+        url.format({
+            hostname: process.env.REACT_APP_DOMAIN,
+            pathname: "/functions/" + pathname,
+            protocol: "https",
+        }),
         { params: { token: await firebase.auth().currentUser.getIdToken() } })).data;
 }
