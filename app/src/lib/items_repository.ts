629cd4821327d2bc15fcfc0fdcb2ff18867fc79c
Yota Toyama <raviqqe@gmail.com>
2017-09-12 10:39:46 +0900

    Create items repository

---
+++ app/src/lib/items_repository.ts
@@ -0,0 +1,43 @@
+import axios from "axios";
+import * as firebase from "firebase";
+import * as _ from "lodash";
+
+import * as json from "./json";
+
+export default class ItemsRepository<A> {
+    private items: A[] | null = null;
+
+    constructor(private name: string, private done: boolean) { }
+
+    public get = async (): Promise<A[]> => {
+        if (_.isArray(this.items)) {
+            return this.items;
+        }
+
+        try {
+            this.items = await json.decode(
+                (await axios.get(await this.reference.getDownloadURL())).data);
+            return this.items;
+        } catch (error) {
+            console.log(error);
+            return [];
+        }
+    }
+
+    public set = async (items: A[]): Promise<void> => {
+        this.items = items;
+        await this.reference.putString(json.encode(items));
+    }
+
+    public create = async (item: A): Promise<void> => {
+        await this.set([item, ...(await this.get())]);
+    }
+
+    private get reference(): firebase.storage.Reference {
+        return firebase.storage().ref(this.path);
+    }
+
+    private get path(): string {
+        return `users/${firebase.auth().currentUser.uid}/${this.name}/${this.done ? "done" : "todo"}`;
+    }
+}
