175718721637551862fcd0d098f093f6e3edeff3
Yota Toyama <raviqqe@gmail.com>
2017-09-13 14:39:43 +0900

    Remove ineffective initialization

--- app/src/setupTests.ts
+++ app/src/setupTests.ts
@@ -1,9 +1,5 @@
 import "web-audio-test-api";
 
-import * as firebase from "./lib/firebase";
-
-firebase.initialize();
-
 class LocalStorageMock {
     // https://stackoverflow.com/questions/32911630/how-do-i-deal-with-localstorage-in-jest-tests
 
