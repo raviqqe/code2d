5652909049e50f572bffdab5e928dc66d6604d69
Yota Toyama <raviqqe@gmail.com>
2017-09-15 21:59:21 +0900

    Remove unused components

--- app/src/component/__tests__/TasksMenu.tsx
+++ app/src/component/__tests__/TasksMenu.tsx
@@ -4,14 +4,14 @@ import { Provider } from "react-redux";
 import { BrowserRouter, Route } from "react-router-dom";
 
 import createStore from "../../redux";
-import Menu from "../Menu";
+import TasksMenu from "../TasksMenu";
 
 it("renders a menu", () => {
     ReactDOM.render(
         <Provider store={createStore()}>
             <BrowserRouter>
                 <div>
-                    <Route exact={true} path="/" render={() => <Menu />} />
+                    <Route exact={true} path="/" render={() => <TasksMenu />} />
                 </div>
             </BrowserRouter>
         </Provider>,
