6d1ae2c7d8b85605214842d887005bdb1b47e33a
Yota Toyama <raviqqe@gmail.com>
2017-09-15 22:19:58 +0900

    Append icons to todo and done buttons

--- app/src/component/ItemsMenu.tsx
+++ app/src/component/ItemsMenu.tsx
@@ -1,4 +1,6 @@
 import * as React from "react";
+import Done = require("react-icons/lib/md/check-box");
+import Todo = require("react-icons/lib/md/check-box-outline-blank");
 import { connect } from "react-redux";
 
 import "./style/ItemsMenu.css";
@@ -18,13 +20,13 @@ export default class extends React.Component<IProps> {
                     className={done ? "ItemsMenu-button" : "ItemsMenu-button-highlighted"}
                     onClick={() => done && toggleItemsState()}
                 >
-                    todo
+                    <Todo /> todo
                 </div>
                 <div
                     className={done ? "ItemsMenu-button-highlighted" : "ItemsMenu-button"}
                     onClick={() => !done && toggleItemsState()}
                 >
-                    done
+                    <Done /> done
                 </div>
                 {children}
             </div>
