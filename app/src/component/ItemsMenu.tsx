b7455c3778ddafbaf8b73dfa2c0b950c7a53994a
Yota Toyama <raviqqe@gmail.com>
2017-09-19 16:30:28 +0900

    Move page buttons into ItemsMenu component

--- app/src/component/ItemsMenu.tsx
+++ app/src/component/ItemsMenu.tsx
@@ -1,8 +1,13 @@
 import * as React from "react";
+import Article = require("react-icons/lib/fa/file-code-o");
+import Book = require("react-icons/lib/go/book");
 import Done = require("react-icons/lib/md/check-box");
 import Todo = require("react-icons/lib/md/check-box-outline-blank");
+import Video = require("react-icons/lib/md/ondemand-video");
+import Task = require("react-icons/lib/md/playlist-add-check");
 import { connect } from "react-redux";
 
+import ItemsMenuButton from "../component/ItemsMenuButton";
 import "./style/ItemsMenu.css";
 
 interface IProps {
@@ -17,22 +22,29 @@ export default class extends React.Component<IProps> {
 
         return (
             <div className="ItemsMenu-container">
-                <div className="ItemsMenu-buttons">
-                    <div
-                        className={done ? "ItemsMenu-button" : "ItemsMenu-button-highlighted"}
-                        onClick={() => done && toggleItemsState()}
-                    >
-                        <Todo /> todo
+                <div className="ItemsMenu-main">
+                    <div className="ItemsMenu-states">
+                        <div
+                            className={done ? "ItemsMenu-state" : "ItemsMenu-state-highlighted"}
+                            onClick={() => done && toggleItemsState()}
+                        >
+                            <Todo /> todo
+                        </div>
+                        <div
+                            className={done ? "ItemsMenu-state-highlighted" : "ItemsMenu-state"}
+                            onClick={() => !done && toggleItemsState()}
+                        >
+                            <Done /> done
+                        </div>
                     </div>
-                    <div
-                        className={done ? "ItemsMenu-button-highlighted" : "ItemsMenu-button"}
-                        onClick={() => !done && toggleItemsState()}
-                    >
-                        <Done /> done
-                    </div>
+                    {!done && createItem}
+                    {children}
                 </div>
-                {!done && createItem}
-                {children}
+                <div className="ItemsMenu-pages">
+                    <ItemsMenuButton path="/tasks"><Task /></ItemsMenuButton>
+                    <ItemsMenuButton path="/articles"><Article /></ItemsMenuButton>
+                    <ItemsMenuButton path="/videos"><Video /></ItemsMenuButton>
+                </div>
             </div>
         );
     }
--- app/src/container/Items.tsx
+++ app/src/container/Items.tsx
@@ -1,12 +1,7 @@
 import * as React from "react";
-import Article = require("react-icons/lib/fa/file-code-o");
-import Book = require("react-icons/lib/go/book");
-import Video = require("react-icons/lib/md/ondemand-video");
-import Todo = require("react-icons/lib/md/playlist-add-check");
 import { connect } from "react-redux";
 import { Link, Redirect } from "react-router-dom";
 
-import ItemsMenuButton from "../component/ItemsMenuButton";
 import "./style/Items.css";
 
 interface IProps {
@@ -27,14 +22,7 @@ class Items extends React.Component<IProps> {
         return (
             <div className="Items-container">
                 <div className="Items-menu-blank" />
-                <div className="Items-menu">
-                    {menu}
-                    <div className="Items-menu-buttons">
-                        <ItemsMenuButton path="/tasks"><Todo /></ItemsMenuButton>
-                        <ItemsMenuButton path="/articles"><Article /></ItemsMenuButton>
-                        <ItemsMenuButton path="/videos"><Video /></ItemsMenuButton>
-                    </div>
-                </div>
+                {menu}
                 <div className="Items-main">
                     {list}
                     <div className="Items-current-item-container">{currentItem || <div />}</div>
