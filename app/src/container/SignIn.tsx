be912e86dc10819faafcfe22e9a57c7943ac3b83
Yota Toyama <raviqqe@gmail.com>
2017-09-20 19:47:03 +0900

    s/main-container/title-container/g

--- app/src/container/SignIn.tsx
+++ app/src/container/SignIn.tsx
@@ -22,7 +22,7 @@ class SignIn extends React.Component<IProps> {
 
         return (
             <div className="SignIn-container">
-                <div className="SignIn-main-container">
+                <div className="SignIn-title-container">
                     <img className="SignIn-icon" src={require("../images/icon.svg")} />
                     <div className="SignIn-title">code2d</div>
                     <div className="SignIn-description">
