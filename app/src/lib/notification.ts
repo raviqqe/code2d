c88bcc8de39ea68f70ebd2c45297fdbb7d9afc79
Yota Toyama <raviqqe@gmail.com>
2017-09-21 22:25:35 +0900

    Add settings duck

--- app/src/lib/notification.ts
+++ app/src/lib/notification.ts
@@ -2,18 +2,27 @@ function isNotificationSupported() {
     return "Notification" in window;
 }
 
-function permission(): "default" | "granted" | "denied" {
-    return (Notification as any).permission;
+export function permission(): boolean | null {
+    switch ((Notification as any).permission) {
+        case "default": return null;
+        case "granted": return true;
+        case "denied": return false;
+    }
+
+    throw new Error("Invalid permission.");
 }
 
-export async function requestPermission(): Promise<void> {
-    if (isNotificationSupported() && permission() === "default") {
+export async function requestPermission(): Promise<boolean | null> {
+    if (isNotificationSupported()) {
         await new Promise((resolve) => Notification.requestPermission(resolve));
+        return permission();
     }
+
+    return false;
 }
 
 export function notify(message: string): void {
-    if (isNotificationSupported() && permission() === "granted") {
+    if (isNotificationSupported() && permission()) {
         const _ = new Notification(message);
     }
 }
---
+++ app/src/redux/settings.ts
@@ -0,0 +1,40 @@
+import { SagaIterator } from "redux-saga";
+import { all, call, put, select } from "redux-saga/effects";
+import Immutable = require("seamless-immutable");
+import { ImmutableObject } from "seamless-immutable";
+import actionCreatorFactory from "typescript-fsa";
+import { reducerWithInitialState } from "typescript-fsa-reducers";
+
+import * as notification from "../lib/notification";
+import { takeEvery } from "./utils";
+
+const actionCreator = actionCreatorFactory("SETTINGS");
+
+const checkNotificationPermission = actionCreator("CHECK_NOTIFICATION_PERMISSION");
+const requestNotificationPermission = actionCreator("REQUEST_NOTIFICATION_PERMISSION");
+const setNotificationState = actionCreator<boolean | null>("SET_NOTIFICATION_STATE");
+
+export const actionCreators = { requestNotificationPermission, setNotificationState };
+
+export interface IState {
+    notificationOn: boolean | null;
+}
+
+export const initialState: ImmutableObject<IState>
+    = Immutable({ notificationOn: null });
+
+export const reducer = reducerWithInitialState(initialState)
+    .case(setNotificationState, (state, notificationOn) => state.merge({ notificationOn }));
+
+export const sagas = [
+    takeEvery(
+        checkNotificationPermission,
+        function* _() {
+            yield put(setNotificationState(notification.permission()));
+        }),
+    takeEvery(
+        requestNotificationPermission,
+        function* _() {
+            yield put(setNotificationState(yield call(notification.requestPermission)));
+        }),
+];
