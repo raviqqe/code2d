78d2ef54b903ce8f1acaba6b812b08c942f20563
Yota Toyama <raviqqe@gmail.com>
2017-09-21 23:11:46 +0900

    Add settings item component

---
+++ app/src/component/SettingsItem.tsx
@@ -0,0 +1,21 @@
+import * as React from "react";
+
+import "./style/SettingsItem.css";
+
+interface IProps {
+    label: string;
+    value: JSX.Element;
+}
+
+export default class extends React.Component<IProps> {
+    public render() {
+        const { label, value } = this.props;
+
+        return (
+            <div className="SettingsItem-container">
+                <div className="SettingsItem-label">{label}</div>
+                {value}
+            </div>
+        );
+    }
+}
