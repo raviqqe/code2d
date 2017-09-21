40e304398bea9dcc57861e89987e6471ac380eab
Yota Toyama <raviqqe@gmail.com>
2017-09-21 20:02:14 +0900

    Add feature images

--- app/src/container/SignIn.tsx
+++ app/src/container/SignIn.tsx
@@ -3,6 +3,7 @@ import GitHub = require("react-icons/lib/go/mark-github");
 import { connect } from "react-redux";
 import { Redirect } from "react-router-dom";
 
+import Feature from "../component/Feature";
 import { actionCreators } from "../redux/sign-in";
 import "./style/SignIn.css";
 
@@ -21,7 +22,7 @@ class SignIn extends React.Component<IProps> {
         }
 
         return (
-            <div>
+            <div className="SignIn-container">
                 <div className="SignIn-main">
                     <div className="SignIn-title-container">
                         <img className="SignIn-icon" src={require("../images/icon.svg")} />
@@ -35,17 +36,27 @@ class SignIn extends React.Component<IProps> {
                     </button>
                 </div>
                 <div className="SignIn-features-paper">
-                    <div>
-                        Manage everything to do in one place.
-                        <div>Tasks to do</div>
-                        <div>Articles to read</div>
-                        <div>Videos to watch</div>
-                        <div>Books to read (WIP)</div>
-                    </div>
-                    <div>
-                        Keep other things away.
-                        Focus on engineering and growing yourself.
-                    </div>
+                    <Feature
+                        title="Manage everything to do"
+                        image={require("../images/tasks.svg")}
+                    >
+                        Tasks to do.
+                        Articles and videos to check.
+                    </Feature>
+                    <Feature
+                        title="Built-in pomodoro timer"
+                        image={require("../images/timer.svg")}
+                    >
+                        Use pomodoro technique
+                        to be productive.
+                    </Feature>
+                    <Feature
+                        title="Focus on engineering"
+                        image={require("../images/focus.svg")}
+                    >
+                        Keep other things away
+                        and grow yourself.
+                    </Feature>
                 </div>
                 <div className="SignIn-footer">
                     <a href="https://github.com/raviqqe/code2d" target="_blank">GitHub</a>
