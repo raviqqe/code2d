217856d067e7dae5a67afa7eee20bbbd8dea06a9
Yota Toyama <raviqqe@gmail.com>
2017-09-21 12:21:39 +0900

    Add feature component

---
+++ app/src/component/Feature.tsx
@@ -0,0 +1,22 @@
+import * as React from "react";
+
+import "./style/Feature.css";
+
+interface IProps {
+    image: string;
+    title: string;
+}
+
+export default class extends React.Component<IProps> {
+    public render() {
+        const { children, image, title } = this.props;
+
+        return (
+            <div className="Feature-container">
+                <img className="Feature-image" src={image} />
+                <div className="Feature-title">{title}</div>
+                <div className="Feature-description">{children}</div>
+            </div>
+        );
+    }
+}
