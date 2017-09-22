6fae21168df70571820183f54bb152532f606d02
Yota Toyama <raviqqe@gmail.com>
2017-09-22 15:07:56 +0900

    Add volume slider (unconnected)

--- app/src/component/Settings.tsx
+++ app/src/component/Settings.tsx
@@ -1,3 +1,5 @@
+import Slider from "rc-slider";
+import "rc-slider/assets/index.css";
 import * as React from "react";
 import { connect } from "react-redux";
 
@@ -6,6 +8,9 @@ import { actionCreators as settingsActionCreators } from "../redux/settings";
 import SettingsItem from "./SettingsItem";
 import "./style/Settings.css";
 import Switch from "./Switch";
+
+const grey = "#bcc";
+const green = "#9db634";
 
 interface IProps {
     deleteAccount: () => void;
@@ -26,6 +31,25 @@ class Settings extends React.Component<IProps> {
                         value={notificationOn
                             ? <div className="Settings-notification-enabled">enabled</div>
                             : <div className="Settings-notification-disabled">disabled</div>
+                        }
+                    />
+                    <SettingsItem
+                        label="Alarm volume"
+                        value={
+                            <div className="Settings-volume-slider">
+                                <Slider
+                                    min={0}
+                                    max={1}
+                                    defaultValue={1}
+                                    step={0.125}
+                                    marks={{ 0: "0", 0.5: "0.5", 1: "1" }}
+                                    railStyle={{ backgroundColor: grey }}
+                                    trackStyle={{ backgroundColor: green }}
+                                    dotStyle={{ borderColor: grey }}
+                                    activeDotStyle={{ borderColor: green }}
+                                    handleStyle={{ borderColor: green, boxShadow: "none" }}
+                                />
+                            </div>
                         }
                     />
                     <div className="Settings-buttons">
