01015dff54fb3247b47da4b360eb58036d9fa1ba
Yota Toyama <raviqqe@gmail.com>
2017-09-25 00:43:44 +0900

    Remove SortableItems component

--- app/src/component/ItemList.tsx
+++ app/src/component/ItemList.tsx
@@ -1,36 +1,75 @@
 import * as React from "react";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+import { arrayMove } from "react-sortable-hoc";
 
-import { isTouchDevice } from "../lib/device";
-import { IItem } from "../lib/items";
-import SortableItems from "./SortableItems";
+import { equal, IItem } from "../lib/items";
+import "./style/ItemList.css";
 
 interface IProps<A extends IItem> {
-    component: any;
+    component: (props) => JSX.Element;
     currentItem: A | null;
     done: boolean;
     fixed?: boolean;
     items: A[];
-    setCurrentItem: (item: A) => void;
     setItems: (args: { done: boolean, items: A[] }) => void;
 }
 
 export default class ItemList<A extends IItem> extends React.Component<IProps<A>> {
     public render() {
         const { component, currentItem, done, fixed, items, setItems } = this.props;
+        const Item = component;
 
         if (items.length === 0) {
             return <div>There is no item.</div>;
         }
 
         return (
-            <SortableItems
-                component={component}
-                currentItem={currentItem}
-                done={done}
-                fixed={fixed}
-                items={items}
-                setItems={setItems}
-            />
+            <DragDropContext
+                onDragEnd={({ destination, source }) => {
+                    if (!destination) {
+                        return;
+                    }
+
+                    setItems({
+                        done,
+                        items: arrayMove(
+                            [...this.props.items],
+                            source.index,
+                            destination.index),
+                    });
+                }}
+            >
+                <Droppable droppableId={`droppable-${done}`} isDropDisabled={fixed}>
+                    {(provided, snapshot) =>
+                        <div ref={provided.innerRef} className="ItemList-container">
+                            {items.map((item) =>
+                                <Draggable
+                                    key={item.id}
+                                    draggableId={item.id}
+                                    isDragDisabled={fixed}
+                                >
+                                    {(provided, snapshot) =>
+                                        <div>
+                                            <div
+                                                className="ItemList-item"
+                                                ref={provided.innerRef}
+                                                style={provided.draggableStyle}
+                                                {...provided.dragHandleProps}
+                                            >
+                                                <Item
+                                                    done={done}
+                                                    highlighted={
+                                                        currentItem && equal(item, currentItem)}
+                                                    {...item}
+                                                />
+                                            </div>
+                                            {provided.placeholder}
+                                        </div>}
+                                </Draggable>)}
+                            {provided.placeholder}
+                        </div>}
+                </Droppable>
+            </DragDropContext>
         );
     }
 }
