e2b02063802ec1d7d787f4486079153d10c1f6e5
Yota Toyama <raviqqe@gmail.com>
2017-09-25 22:40:03 +0900

    Refactor lib/audio

--- app/src/lib/audio.ts
+++ app/src/lib/audio.ts
@@ -1,8 +1,7 @@
 import Pizzicato = require("pizzicato");
 
 export function playAlarm(volume: number = 1): void {
-    const sound = new Pizzicato.Sound({ source: "wave", options: { release: 3 } });
-    sound.volume = volume;
+    const sound = new Pizzicato.Sound({ source: "wave", options: { release: 3, volume } });
     sound.play();
     setTimeout(() => sound.stop(), 500);
 }
