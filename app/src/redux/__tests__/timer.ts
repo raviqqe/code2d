9645471a3367f7fbb1c929d4cad23fdd55383e62
Yota Toyama <raviqqe@gmail.com>
2017-09-25 22:17:19 +0900

    Test timer alarm

--- app/src/redux/__tests__/timer.ts
+++ app/src/redux/__tests__/timer.ts
@@ -1,3 +1,6 @@
+import createStore from "..";
+import * as audio from "../../lib/audio";
+import { dispatch } from "../../lib/utils";
 import { actionCreators, initialState, reducer } from "../timer";
 
 it("toggles a timer", () => {
@@ -7,4 +10,12 @@ it("toggles a timer", () => {
 
     expect(state.on).toBe(true);
     expect(reducer(state, actionCreators.toggleTimer()).on).toBe(false);
+});
+
+it("plays alarm", async () => {
+    const spy = jest.spyOn(audio, "playAlarm");
+    const store = createStore();
+
+    await dispatch(store, actionCreators.playAlarm());
+    expect(spy).toHaveBeenCalled();
 });
