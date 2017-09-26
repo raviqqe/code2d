5a9b5dc70bbc593e469552eba1221d794c2f2aff
Yota Toyama <raviqqe@gmail.com>
2017-09-26 14:08:52 +0900

    Test message duck

---
+++ app/src/redux/__tests__/message.ts
@@ -0,0 +1,41 @@
+import createStore from "..";
+import { dispatch } from "../../lib/utils";
+import { actionCreators, initialState, reducer } from "../message";
+
+jest.useFakeTimers();
+
+function getState(store): typeof initialState {
+    return store.getState().message;
+}
+
+it("sets and clears a current message", () => {
+    expect(initialState.message).toBe("");
+    let state = reducer(initialState, actionCreators.sendMessage("Hello, world!"));
+    expect(state.message).toBe("Hello, world!");
+    state = reducer(state, actionCreators.clearMessage());
+    expect(state.message).toBe("");
+});
+
+it("sends message", async () => {
+    expect.assertions(2);
+
+    const store = createStore();
+
+    expect(initialState.message).toBe("");
+    const promise = dispatch(store, actionCreators.sendMessage("Hello, world!"));
+    jest.runAllTimers();
+    await promise;
+    expect(getState(store).message).toBe("");
+});
+
+it("sends a non-temporary message", async () => {
+    expect.assertions(2);
+
+    const store = createStore();
+
+    expect(initialState.message).toBe("");
+    const promise = dispatch(store, actionCreators.sendMessage("Hello, world!", { temporary: false }));
+    jest.runAllTimers();
+    await promise;
+    expect(getState(store).message).toBe("Hello, world!");
+});
