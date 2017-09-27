7a47c37602d21ff5e6c9218620ab3e2bc6152682
Yota Toyama <raviqqe@gmail.com>
2017-09-27 12:03:35 +0900

    Test rendering of items pages

--- app/src/container/__tests__/Articles.tsx
+++ app/src/container/__tests__/Articles.tsx
@@ -4,14 +4,14 @@ import { Provider } from "react-redux";
 import { BrowserRouter, Route } from "react-router-dom";
 
 import createStore from "../../redux";
-import Tasks from "../Tasks";
+import Articles from "../Articles";
 
-it("renders a todo tasks page", () => {
+it("renders a articles page", () => {
     ReactDOM.render(
         <Provider store={createStore()}>
             <BrowserRouter>
                 <div>
-                    <Route exact={true} path="/" render={() => <Tasks />} />
+                    <Route exact={true} path="/" render={() => <Articles />} />
                     <Route exact={true} path="/sign-in" render={() => <div>sign-in page mock</div>} />
                 </div>
             </BrowserRouter>
