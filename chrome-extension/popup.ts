677a2eb712b6d061c127d41135088c777b742a53
Yota Toyama <raviqqe@gmail.com>
2017-09-28 15:56:39 +0900

    Use typescript

--- chrome-extension/popup.ts
+++ chrome-extension/popup.ts
@@ -1,22 +1,6 @@
-// Copyright (c) 2014 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-/**
- * Get the current URL.
- *
- * @param {function(string)} callback called when the URL of the current tab
- *   is found.
- */
 function getCurrentTabUrl(callback) {
-  // Query filter to be passed to chrome.tabs.query - see
-  // https://developer.chrome.com/extensions/tabs#method-query
-  var queryInfo = {
-    active: true,
-    currentWindow: true
-  };
 
-  chrome.tabs.query(queryInfo, (tabs) => {
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     // chrome.tabs.query invokes the callback with a list of tabs that match the
     // query. When the popup is opened, there is certainly a window and at least
     // one tab, so we can safely assume that |tabs| is a non-empty array.
---
+++ chrome-extension/rollup.config.js
@@ -0,0 +1,10 @@
+import typescript from 'rollup-plugin-typescript'
+
+export default {
+  input: 'popup.ts',
+  output: {
+    file: 'popup.js',
+    format: 'es'
+  },
+  plugins: [ typescript() ]
+}
