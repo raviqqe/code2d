da41d07ea3c68d9941434a55b477169685e3c0ae
Yota Toyama <raviqqe@gmail.com>
2017-09-28 19:50:07 +0900

    Build icons

--- chrome-extension/rakefile.rb
+++ chrome-extension/rakefile.rb
@@ -1,12 +1,16 @@
+directory 'images'
+
 task :deps do
   sh 'npm install'
 end
 
-task build: :deps do
+task build: %i[deps images] do
   sh 'npx rollup -c'
 
-  sh %w[inkscape
-        --export-width 19 --export-height 19
-        --export-png icon.png
-        ../images/icon.svg].join ' '
+  [16, 19, 48, 128].each do |size|
+    sh %W[inkscape
+          --export-width #{size} --export-height #{size}
+          --export-png images/icon_#{size}.png
+          ../images/icon.svg].join ' '
+  end
 end
