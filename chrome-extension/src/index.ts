4a52d26095a336c2d1a2421b9ee05cca2abdcdcc
Yota Toyama <raviqqe@gmail.com>
2017-09-30 14:41:26 +0900

    Add item from chrome extension

--- chrome-extension/src/index.ts
+++ chrome-extension/src/index.ts
@@ -1,5 +1,6 @@
+import axios from "axios";
 import * as firebase from "firebase";
-import * as url from "url";
+import { format } from "url";
 
 const config = require("../config.json");
 
@@ -15,6 +16,21 @@ async function getCurrentTabUrl(): Promise<string> {
             (tabs) => resolve(tabs[0].url))) as string;
 }
 
+async function addItem(): Promise<void> {
+    await axios.get(
+        format({
+            hostname: `us-central1-${config.firebase.projectId}.cloudfunctions.net`,
+            pathname: "/addItem",
+            protocol: "https",
+        }),
+        {
+            headers: {
+                Authorization: `Bearer ${await firebase.auth().currentUser.getIdToken()}`,
+            },
+            params: { url: await getCurrentTabUrl() },
+        });
+}
+
 document.addEventListener("DOMContentLoaded", async () => {
     const button = document.getElementById("sign-in-button") as HTMLButtonElement;
     const message = document.getElementById("message");
@@ -26,14 +42,22 @@ document.addEventListener("DOMContentLoaded", async () => {
         await firebase.auth().signInWithPopup(new firebase.auth.GithubAuthProvider());
     });
 
-    firebase.auth().onAuthStateChanged((user) => {
+    firebase.auth().onAuthStateChanged(async (user) => {
         if (user === null) {
             button.style.display = "initial";
             message.style.display = "none";
         } else {
             button.style.display = "none";
             message.style.display = "initial";
-            message.appendChild(document.createTextNode(signingIn ? "Signed in!" : "Item added"));
+
+            if (signingIn) {
+                message.appendChild(document.createTextNode("Signed in"));
+            } else {
+                message.appendChild(document.createTextNode("Adding item..."));
+                await addItem();
+                message.appendChild(document.createTextNode("Item added"));
+            }
+
             setTimeout(() => window.close(), 5000);
         }
     });
