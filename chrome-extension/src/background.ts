377f0340a86642eee60ff6b505eb15e9b148e05d
Yota Toyama <raviqqe@gmail.com>
2017-10-03 22:55:52 +0900

    Renew chrome extension

--- chrome-extension/src/background.ts
+++ chrome-extension/src/background.ts
@@ -1,3 +1,39 @@
-chrome.browserAction.onClicked.addListener((tab) => {
-    chrome.tabs.sendMessage(tab.id, {});
+import axios from "axios";
+import * as firebase from "firebase";
+import { format } from "url";
+
+const config = require("../config.json");
+
+firebase.initializeApp({
+    apiKey: config.firebase.apiKey,
+    authDomain: `${config.firebase.projectId}.firebaseapp.com`,
 });
+
+let signedIn = false;
+
+chrome.browserAction.onClicked.addListener(async (tab) => {
+    if (signedIn) {
+        chrome.tabs.sendMessage(tab.id, {});
+    } else {
+        await firebase.auth().signInWithPopup(new firebase.auth.GithubAuthProvider());
+    }
+});
+
+chrome.runtime.onMessage.addListener(async ({ url }, _, sendResponse) => {
+    await axios.get(
+        format({
+            hostname: `us-central1-${config.firebase.projectId}.cloudfunctions.net`,
+            pathname: "/addItem",
+            protocol: "https",
+        }),
+        {
+            headers: {
+                Authorization: `Bearer ${await firebase.auth().currentUser.getIdToken()}`,
+            },
+            params: { url },
+        });
+
+    sendResponse(true);
+});
+
+firebase.auth().onAuthStateChanged(async (user) => signedIn = user !== null);
--- chrome-extension/src/content-script.tsx
+++ chrome-extension/src/content-script.tsx
@@ -1,46 +1,10 @@
-import axios from "axios";
-import * as firebase from "firebase";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import * as url from "url";
 
-const config = require("../config.json");
-
-firebase.initializeApp({
-    apiKey: config.firebase.apiKey,
-    authDomain: `${config.firebase.projectId}.firebaseapp.com`,
-});
-
-async function addItem(): Promise<void> {
-    await axios.get(
-        url.format({
-            hostname: `us-central1-${config.firebase.projectId}.cloudfunctions.net`,
-            pathname: "/addItem",
-            protocol: "https",
-        }),
-        {
-            headers: {
-                Authorization: `Bearer ${await firebase.auth().currentUser.getIdToken()}`,
-            },
-            params: { url: window.location.href },
-        });
-}
+import App from "./app";
 
 const root = document.createElement("div");
-root.style.visibility = "hidden";
-root.style.transform = "translateY(-200%)";
-root.style.transition = "visibility 0.1s, transform 0.1s";
-root.style.background = "white";
-root.style["border-radius"] = "0.5em";
-root.style["box-shadow"] = "0.5em 0.5em 1em rgba(0, 0, 0, 0.5)";
 root.style.position = "fixed";
-root.style.top = "1em";
-root.style.right = "1em";
-
 document.body.appendChild(root);
 
-chrome.runtime.onMessage.addListener(() => {
-    root.style.visibility = "visible";
-    root.style.transform = "translateY(0)";
-    ReactDOM.render(<div>Hello, world!</div>, root);
-});
+ReactDOM.render(<App />, root);
