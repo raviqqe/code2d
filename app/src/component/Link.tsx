d9f94400821764006307ab73d12d2fa05172f4bd
Yota Toyama <raviqqe@gmail.com>
2017-10-04 20:20:03 +0900

    Add Link component

---
+++ app/src/component/Link.tsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+
+import "./style/Link.css";
+
+interface IProps {
+    href: string;
+}
+
+export default class extends React.Component<IProps> {
+    public render() {
+        const { children, href } = this.props;
+
+        return <a className="Link-container" href={href} target="_blank">{children}</a>;
+    }
+}
