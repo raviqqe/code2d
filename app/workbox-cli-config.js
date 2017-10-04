9b3f8950b07362bcc79033ab16dd797e67c414b0
Yota Toyama <raviqqe@gmail.com>
2017-10-04 19:43:10 +0900

    Don't cache 404.html file

--- app/workbox-cli-config.js
+++ app/workbox-cli-config.js
@@ -1,10 +1,6 @@
 module.exports = {
-  "globDirectory": "build/",
-  "globPatterns": [
-    "**/*.{html,png,json,pdf,js,css,map,svg,woff,eot,ttf}"
-  ],
-  "swDest": "build/service-worker.js",
-  "globIgnores": [
-    "../workbox-cli-config.js"
-  ]
-};
+  'globDirectory': 'build/',
+  'globPatterns': ['**/*.{html,png,json,pdf,js,css,map,svg,woff,eot,ttf}'],
+  'swDest': 'build/service-worker.js',
+  'globIgnores': ['404.html']
+}
