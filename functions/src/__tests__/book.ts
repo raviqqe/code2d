a97b0b0d95d7181fa7c51f07c5e7c7ea18979e2e
Yota Toyama <raviqqe@gmail.com>
2017-10-06 18:29:03 +0900

    Test more properties of book objects

--- functions/src/__tests__/book.ts
+++ functions/src/__tests__/book.ts
@@ -1,10 +1,16 @@
+import is = require("is_js");
+
 import { convertUrlIntoBook } from "../book";
 
 test("Convert a URL into a book object", async () => {
-    expect.assertions(2);
+    expect.assertions(6);
 
     const book = await convertUrlIntoBook("https://books.rakuten.co.jp/rb/15092688/");
 
     expect(typeof book.name).toBe("string");
     expect(book.title).toBeUndefined();
+    expect(typeof book.author).toBe("string");
+    expect(typeof book.publisher).toBe("string");
+    expect(is.url(book.imageUrl)).toBe(true);
+    expect(is.url(book.pageUrl)).toBe(true);
 });
