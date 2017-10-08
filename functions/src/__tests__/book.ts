61fc9c7ccbf21ef4a9c91c8e56e384430a346a1d
Yota Toyama <raviqqe@gmail.com>
2017-10-08 11:02:21 +0900

    Handle Amazon book URLs

--- functions/src/__tests__/book.ts
+++ functions/src/__tests__/book.ts
@@ -3,14 +3,24 @@ import is = require("is_js");
 import { convertUrlIntoBook } from "../book";
 
 test("Convert a URL into a book object", async () => {
-    expect.assertions(6);
+    expect.assertions(12);
 
-    const book = await convertUrlIntoBook("https://books.rakuten.co.jp/rb/15092688/");
+    for (const url of [
+        "https://books.rakuten.co.jp/rb/15092688/",
+        "https://www.amazon.co.jp/gp/product/4873117143/ref=s9u_simh_gw_i2" +
+        "?ie=UTF8&pd_rd_i=4873117143" +
+        "&pd_rd_r=14eb393f-abcb-11e7-ab93-378794d3c17e" +
+        "&pd_rd_w=EhfK6&pd_rd_wg=yjLAe&pf_rd_m=AN1VRQENFRJN5" +
+        "&pf_rd_s=&pf_rd_r=6S1GM48Y0XW3X9GT3YHN&pf_rd_t=36701" +
+        "&pf_rd_p=d4802771-73ad-49b1-a154-90aaec384d3e&pf_rd_i=desktop",
+    ]) {
+        const book = await convertUrlIntoBook(url);
 
-    expect(typeof book.name).toBe("string");
-    expect(book.title).toBeUndefined();
-    expect(typeof book.author).toBe("string");
-    expect(typeof book.publisher).toBe("string");
-    expect(is.url(book.image)).toBe(true);
-    expect(is.url(book.url)).toBe(true);
+        expect(typeof book.name).toBe("string");
+        expect(book.title).toBeUndefined();
+        expect(typeof book.author).toBe("string");
+        expect(typeof book.publisher).toBe("string");
+        expect(is.url(book.image)).toBe(true);
+        expect(is.url(book.url)).toBe(true);
+    }
 });
--- functions/src/book.ts
+++ functions/src/book.ts
@@ -1,13 +1,19 @@
 import axios from "axios";
 import cheerio = require("cheerio");
 import { Request, Response } from "express";
+import { parse } from "url";
 
 import { callApi } from "./rakuten";
 import { httpsFunction } from "./utils";
 
 async function convertUrlIntoIsbn(url: string): Promise<string> {
-    return cheerio.load((await axios.get(url)).data)
-        ("meta[property=\"books:isbn\"]").attr("content");
+    const { data } = await axios.get(url);
+
+    if (parse(url).hostname.match(/amazon/)) {
+        return data.match(/<li><b>ISBN-10:<\/b> *([0-9X]+) *<\/li>/i)[1];
+    }
+
+    return cheerio.load(data)("meta[property=\"books:isbn\"]").attr("content");
 }
 
 export async function convertUrlIntoBook(url: string): Promise<any> {
