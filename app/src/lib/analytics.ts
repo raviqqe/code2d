a6068511b036c21e10e9c91e02c45dc25f473a2a
Yota Toyama <raviqqe@gmail.com>
2017-10-09 20:02:13 +0900

    Log client-side exceptions

--- app/src/lib/analytics.ts
+++ app/src/lib/analytics.ts
@@ -2,6 +2,8 @@ import * as reactGa from "react-ga";
 
 import config from "../config";
 
+window.onerror = (description) => reactGa.exception({ description });
+
 export function initialize(): void {
     reactGa.initialize(config.google.analytics.trackingId, { titleCase: false });
 }
