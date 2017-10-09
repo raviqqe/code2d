8c47e1a5ee6821dd742960c82c0ace77eef37638
Yota Toyama <raviqqe@gmail.com>
2017-10-09 23:50:14 +0900

    Test convertUrlIntoArticle function

--- functions/src/__tests__/article.ts
+++ functions/src/__tests__/article.ts
@@ -1,4 +1,6 @@
-import { convertIntoUrl } from "../article";
+import is = require("is_js");
+
+import { convertIntoUrl, convertUrlIntoArticle } from "../article";
 
 const baseUrl = "https://foo.com";
 
@@ -10,4 +12,21 @@ test("Convert URL-like strings into complete URLs", () => {
 test("Don't convert falsy values into URLs", () => {
     expect(convertIntoUrl(null, baseUrl)).toBe(null);
     expect(convertIntoUrl(undefined, baseUrl)).toBe(undefined);
+});
+
+test("Convert a URL into an article object", async () => {
+    expect.assertions(8);
+
+    for (const articleUrl of [
+        "https://martinfowler.com/bliki/MonolithFirst.html",
+        "https://charlieharvey.org.uk/page/javascript_the_weird_parts",
+    ]) {
+        const { date, favicon, image, name, text, url }
+            = await convertUrlIntoArticle(articleUrl);
+
+        expect(is.url(image)).toBe(true);
+        expect(typeof name).toBe("string");
+        expect(typeof text).toBe("string");
+        expect(is.url(url)).toBe(true);
+    }
 });
--- functions/src/__tests__/book.ts
+++ functions/src/__tests__/book.ts
@@ -5,7 +5,7 @@ import { convertUrlIntoBook } from "../book";
 test("Convert a URL into a book object", async () => {
     expect.assertions(12);
 
-    for (const url of [
+    for (const bookUrl of [
         "https://books.rakuten.co.jp/rb/15092688/",
         "https://www.amazon.co.jp/gp/product/4873117143/ref=s9u_simh_gw_i2" +
         "?ie=UTF8&pd_rd_i=4873117143" +
@@ -14,13 +14,14 @@ test("Convert a URL into a book object", async () => {
         "&pf_rd_s=&pf_rd_r=6S1GM48Y0XW3X9GT3YHN&pf_rd_t=36701" +
         "&pf_rd_p=d4802771-73ad-49b1-a154-90aaec384d3e&pf_rd_i=desktop",
     ]) {
-        const book = await convertUrlIntoBook(url);
+        const { author, image, name, publisher, title, url }
+            = await convertUrlIntoBook(bookUrl);
 
-        expect(typeof book.name).toBe("string");
-        expect(book.title).toBeUndefined();
-        expect(typeof book.author).toBe("string");
-        expect(typeof book.publisher).toBe("string");
-        expect(is.url(book.image)).toBe(true);
-        expect(is.url(book.url)).toBe(true);
+        expect(typeof name).toBe("string");
+        expect(title).toBeUndefined();
+        expect(typeof author).toBe("string");
+        expect(typeof publisher).toBe("string");
+        expect(is.url(image)).toBe(true);
+        expect(is.url(url)).toBe(true);
     }
 });
