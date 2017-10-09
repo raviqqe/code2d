9c974cd719cf03462f90dcc79693770d8a02b78a
Yota Toyama <raviqqe@gmail.com>
2017-10-09 19:33:41 +0900

    Mock react-ga

---
+++ app/__mocks__/react-ga.ts
@@ -0,0 +1,27 @@
+export function initialize() {
+    // Do nothing.
+}
+
+export function set() {
+    // Do nothing.
+}
+
+export function pageview() {
+    // Do nothing.
+}
+
+export function modalview() {
+    // Do nothing.
+}
+
+export function event() {
+    // Do nothing.
+}
+
+export function timing() {
+    // Do nothing.
+}
+
+export function exception() {
+    // Do nothing.
+}
