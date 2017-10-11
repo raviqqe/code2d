fcbf4ac2aa83263de46bfd5102bd6edcc3ad0b26
Yota Toyama <raviqqe@gmail.com>
2017-10-11 09:50:34 +0900

    Refactor book module test

--- functions/src/__tests__/book.ts
+++ functions/src/__tests__/book.ts
@@ -4,6 +4,15 @@ import { convertUrlIntoBook } from "../book";
 
 jest.setTimeout(20000);
 
+function validateBook({ author, image, name, publisher, title, url }): void {
+    expect(typeof name).toBe("string");
+    expect(title).toBeUndefined();
+    expect(typeof author).toBe("string");
+    expect(typeof publisher).toBe("string");
+    expect(is.url(image)).toBe(true);
+    expect(is.url(url)).toBe(true);
+}
+
 test("Convert URLs of English books into book objects", async () => {
     expect.assertions(12);
 
@@ -11,15 +20,7 @@ test("Convert URLs of English books into book objects", async () => {
         "https://www.betterworldbooks.com/it-id-1501142976.aspx",
         "https://www.betterworldbooks.com/The-Power-of-Now-id-1577314808.aspx",
     ]) {
-        const { author, image, name, publisher, title, url }
-            = await convertUrlIntoBook(bookUrl);
-
-        expect(typeof name).toBe("string");
-        expect(title).toBeUndefined();
-        expect(typeof author).toBe("string");
-        expect(typeof publisher).toBe("string");
-        expect(is.url(image)).toBe(true);
-        expect(is.url(url)).toBe(true);
+        validateBook(await convertUrlIntoBook(bookUrl));
     }
 });
 
@@ -30,14 +31,6 @@ test("Convert URLs of Japanese books into book objects", async () => {
         "https://books.rakuten.co.jp/rb/14920954/",
         "https://books.rakuten.co.jp/rb/15078428/",
     ]) {
-        const { author, image, name, publisher, title, url }
-            = await convertUrlIntoBook(bookUrl, { country: "JP" });
-
-        expect(typeof name).toBe("string");
-        expect(title).toBeUndefined();
-        expect(typeof author).toBe("string");
-        expect(typeof publisher).toBe("string");
-        expect(is.url(image)).toBe(true);
-        expect(is.url(url)).toBe(true);
+        validateBook(await convertUrlIntoBook(bookUrl, { country: "JP" }));
     }
 });
