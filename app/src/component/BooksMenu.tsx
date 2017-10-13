8eb5c6ca34d5cab87eb4aca656e9a5232c6574d3
Yota Toyama <raviqqe@gmail.com>
2017-10-14 02:04:22 +0900

    Show trending books

--- app/src/component/BooksMenu.tsx
+++ app/src/component/BooksMenu.tsx
@@ -1,16 +1,23 @@
 import * as React from "react";
+import { connect } from "react-redux";
 
+import { IBook } from "../lib/books";
 import CreateBook from "./CreateBook";
 import ItemsMenu from "./ItemsMenu";
+import SimpleBook from "./SimpleBook";
 import TopSalesBooks from "./TopSalesBooks";
+import TrendingItems from "./TrendingItems";
 
 interface IProps {
     done: boolean;
     onItemsStateChange: (done: boolean) => void;
+    trendingItems: IBook[];
 }
 
-export default class extends React.Component<IProps> {
+class BooksMenu extends React.Component<IProps> {
     public render() {
+        const { trendingItems } = this.props;
+
         return (
             <ItemsMenu
                 {...this.props}
@@ -18,8 +25,11 @@ export default class extends React.Component<IProps> {
                 doneButtonText="read"
                 todoButtonText="to read"
             >
+                <TrendingItems itemComponent={SimpleBook} trendingItems={trendingItems} />
                 <TopSalesBooks />
             </ItemsMenu>
         );
     }
 }
+
+export default connect(({ books }) => books)(BooksMenu);
