58e4dbde0e9120c7c2abcf8fa1e8f454ac96688a
Yota Toyama <raviqqe@gmail.com>
2017-10-13 11:02:24 +0900

    Filter books which are not interesting

--- functions/src/top-sales-books.ts
+++ functions/src/top-sales-books.ts
@@ -5,7 +5,8 @@ import { callApi } from "./rakuten";
 import { httpsFunction } from "./utils";
 
 export async function getTopSalesBooks(): Promise<any[]> {
-    return await callApi("Book", { booksGenreId: "001005", sort: "sales" });
+    return (await callApi("Book", { booksGenreId: "001005", sort: "sales" }))
+        .filter(({ name }) => !["LINE", "年賀状"].some((pattern) => name.includes(pattern)));
 }
 
 export default httpsFunction(async (_, response: Response) => {
