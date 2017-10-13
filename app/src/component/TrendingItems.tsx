314ecb9778db65e9f60b91e464f91016ad76d31b
Yota Toyama <raviqqe@gmail.com>
2017-10-13 23:07:52 +0900

    Add trending items component

---
+++ app/src/component/TrendingItems.tsx
@@ -0,0 +1,33 @@
+import * as React from "react";
+
+import { IItem } from "../lib/items";
+import Modal from "./Modal";
+import "./style/TrendingItems.css";
+
+interface IProps<A extends IItem> {
+    itemComponent: (props) => JSX.Element;
+    trendingItems: A[];
+}
+
+export default class TrendingItems<A extends IItem> extends React.Component<IProps<A>> {
+    public render() {
+        const { itemComponent, trendingItems } = this.props;
+        const Item = itemComponent;
+
+        return (
+            <Modal
+                button={
+                    ({ shown, showWindow }) =>
+                        <div className="TrendingItems-button" onClick={showWindow}>
+                            Trending
+                        </div>}
+                showCloseButton={true}
+            >
+                <div className="TrendingItems-container">
+                    {trendingItems.map((item: A, index) =>
+                        <Item key={index} {...item} />)}
+                </div>
+            </Modal>
+        );
+    }
+}
