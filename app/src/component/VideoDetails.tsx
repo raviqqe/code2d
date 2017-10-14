10da29e0d7d8b22aab684aabc6ff705d16ab836a
Yota Toyama <raviqqe@gmail.com>
2017-10-14 15:09:49 +0900

    Improve style of trending videos

--- app/src/component/VideoDetails.tsx
+++ app/src/component/VideoDetails.tsx
@@ -12,20 +12,27 @@ export default class extends React.Component<IProps> {
     public render() {
         const { description, detailed, embedUrl, publishedAt } = this.props;
 
-        return [
-            (
-                <div key="video" className="VideoDetails-video">
-                    <iframe
-                        id="ytplayer"
-                        src={embedUrl}
-                        frameBorder="0"
-                        allowFullScreen={true}
-                    />
-                </div>
-            ),
+        const video = (
+            <div key="video" className="VideoDetails-video">
+                <iframe
+                    id="ytplayer"
+                    src={embedUrl}
+                    frameBorder="0"
+                    allowFullScreen={true}
+                />
+            </div>
+        );
+
+        const details = [
             detailed && description &&
             <div key="description" className="VideoDetails-description">{description}</div>,
             <LabeledDate key="publishedOn" label="Published on" value={publishedAt} />,
         ];
+
+        if (detailed) {
+            return [video, ...details];
+        }
+
+        return [...details, video];
     }
 }
