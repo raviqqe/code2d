cc567edb5c795f49e0b19d2970bba1b05c602919
Yota Toyama <raviqqe@gmail.com>
2017-10-14 14:21:47 +0900

    Show trending articles

--- app/src/component/ArticlesMenu.tsx
+++ app/src/component/ArticlesMenu.tsx
@@ -1,22 +1,33 @@
 import * as React from "react";
+import { connect } from "react-redux";
 
+import { IArticle } from "../lib/articles";
 import CreateArticle from "./CreateArticle";
 import ItemsMenu from "./ItemsMenu";
+import SimpleArticle from "./SimpleArticle";
+import TrendingItems from "./TrendingItems";
 
 interface IProps {
     done: boolean;
     onItemsStateChange: (done: boolean) => void;
+    trendingItems: IArticle[];
 }
 
-export default class extends React.Component<IProps> {
+class ArticlesMenu extends React.Component<IProps> {
     public render() {
+        const { trendingItems } = this.props;
+
         return (
             <ItemsMenu
                 {...this.props}
                 createItem={<CreateArticle />}
                 doneButtonText="read"
                 todoButtonText="to read"
-            />
+            >
+                <TrendingItems itemComponent={SimpleArticle} trendingItems={trendingItems} />
+            </ItemsMenu>
         );
     }
 }
+
+export default connect(({ articles }) => articles)(ArticlesMenu);
