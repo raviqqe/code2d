52ca4fc1b2668aa60b45e756995dd4864eeb889d
Yota Toyama <raviqqe@gmail.com>
2017-10-14 14:55:46 +0900

    Show trending videos

--- app/src/component/VideosMenu.tsx
+++ app/src/component/VideosMenu.tsx
@@ -1,22 +1,33 @@
 import * as React from "react";
+import { connect } from "react-redux";
 
+import { IVideo } from "../lib/videos";
 import CreateVideo from "./CreateVideo";
 import ItemsMenu from "./ItemsMenu";
+import SimpleVideo from "./SimpleVideo";
+import TrendingItems from "./TrendingItems";
 
 interface IProps {
     done: boolean;
     onItemsStateChange: (done: boolean) => void;
+    trendingItems: IVideo[];
 }
 
-export default class extends React.Component<IProps> {
+class VideosMenu extends React.Component<IProps> {
     public render() {
+        const { trendingItems } = this.props;
+
         return (
             <ItemsMenu
                 {...this.props}
                 createItem={<CreateVideo />}
                 doneButtonText="watched"
                 todoButtonText="to watch"
-            />
+            >
+                <TrendingItems itemComponent={SimpleVideo} trendingItems={trendingItems} />
+            </ItemsMenu>
         );
     }
 }
+
+export default connect(({ videos }) => videos)(VideosMenu);
