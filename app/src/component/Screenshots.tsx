5be181f18935e81e034ca86367870980532ce932
Yota Toyama <raviqqe@gmail.com>
2017-10-18 23:36:01 +0900

    Link screenshots

--- app/src/component/Screenshots.tsx
+++ app/src/component/Screenshots.tsx
@@ -5,15 +5,23 @@ import "slick-carousel/slick/slick.css";
 
 import "./style/Screenshots.css";
 
+const images: string[] = [
+    require("../images/screenshots/tasks.png"),
+    require("../images/screenshots/articles.png"),
+    require("../images/screenshots/videos.png"),
+];
+
 export default class extends React.Component {
     public render() {
         return (
             <div className="Screenshots-container">
                 <div className="Screenshots-title">Screenshots</div>
                 <Slider dots={true} adaptiveHeight={true} arrows={false} autoplay={true}>
-                    <img src={require("../images/screenshots/tasks.png")} />
-                    <img src={require("../images/screenshots/articles.png")} />
-                    <img src={require("../images/screenshots/videos.png")} />
+                    {images.map((path) =>
+                        // Image component is not available here somehow.
+                        <a key={path} href={path} target="_blank">
+                            <img className="Screenshots-image" src={path} />
+                        </a>)}
                 </Slider>
             </div>
         );
