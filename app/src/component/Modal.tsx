62cd375fecf77b02656453ceb4da1aed6b7aa0a3
Yota Toyama <raviqqe@gmail.com>
2017-10-20 23:12:08 +0900

    Use portal API in modal component

--- app/src/component/Modal.tsx
+++ app/src/component/Modal.tsx
@@ -1,8 +1,12 @@
 import * as React from "react";
+import * as ReactDOM from "react-dom";
 import Close = require("react-icons/lib/md/close");
 
+import config from "../config";
 import Button from "./Button";
 import "./style/Modal.css";
+
+const animationDelayMs = 200;
 
 interface IProps {
     button: (props: { opened: boolean, openWindow: () => void }) => JSX.Element;
@@ -16,45 +20,67 @@ interface IState {
 
 export default class extends React.Component<IProps, IState> {
     public state: IState = { opened: false };
+    private element: HTMLElement | null;
 
     public render() {
-        const { button, children, showCloseButton } = this.props;
-        const ShowButton = button;
+        const Button = this.props.button;
         const { opened } = this.state;
-        const closeWindow = () => this.setState({ opened: false });
 
-        return (
-            <div>
-                <ShowButton opened={opened} openWindow={() => this.setState({ opened: true })} />
-                <div
-                    className={"Modal-container" + (opened ? "" : "-hidden")}
-                    onClick={closeWindow}
-                >
-                    {showCloseButton &&
-                        <div className="Modal-close-button-container">
-                            <Button
-                                className="Modal-close-button"
-                                onClick={closeWindow}
-                            >
-                                <Close />
-                            </Button>
-                        </div>}
-                    <div
-                        className="Modal-window"
-                        onClick={(event) => event.stopPropagation()}
-                    >
-                        {typeof children === "function" ? children(closeWindow) : children}
-                    </div>
-                </div>
-            </div>
-        );
+        return [
+            (
+                <Button
+                    key="button"
+                    opened={opened}
+                    openWindow={() => this.setState({ opened: true })}
+                />
+            ),
+            opened && this.renderModalWindow(),
+        ];
+    }
+
+    public componentWillUpdate(_, { opened }: IState) {
+        if (!this.state.opened && opened) {
+            this.element = document.createElement("div");
+            document.getElementById(config.rootId).appendChild(this.element);
+        }
     }
 
     public componentDidUpdate(_, { opened }: IState) {
         const { onOpen } = this.props;
 
-        if (onOpen && !opened && this.state.opened) {
-            setTimeout(onOpen, 200); // Run after animation.
+        if (!opened && this.state.opened && onOpen) {
+            setTimeout(onOpen, animationDelayMs);
+        } else if (opened && !this.state.opened) {
+            this.element.remove();
+            this.element = null;
         }
+    }
+
+    public componentWillUnmount() {
+        if (this.element) {
+            this.element.remove();
+        }
+    }
+
+    private renderModalWindow = (): JSX.Element => {
+        const { children, showCloseButton } = this.props;
+        const closeWindow = () => this.setState({ opened: false });
+
+        return (ReactDOM as any).createPortal(
+            <div className="Modal-container" onClick={closeWindow}>
+                {showCloseButton &&
+                    <div className="Modal-close-button-container">
+                        <Button className="Modal-close-button" onClick={closeWindow}>
+                            <Close />
+                        </Button>
+                    </div>}
+                <div
+                    className="Modal-window"
+                    onClick={(event) => event.stopPropagation()}
+                >
+                    {typeof children === "function" ? children(closeWindow) : children}
+                </div>
+            </div>,
+            this.element);
     }
 }
