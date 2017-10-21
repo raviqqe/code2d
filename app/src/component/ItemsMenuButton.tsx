ab9d203b43c1f4f28bf7437865bb77020655f587
Yota Toyama <raviqqe@gmail.com>
2017-10-21 20:14:51 +0900

    Use circle button component as items menu button

--- app/src/component/ItemsMenuButton.tsx
+++ app/src/component/ItemsMenuButton.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import Menu = require("react-icons/lib/md/menu");
 
+import CircleButton from "./CircleButton";
 import "./style/ItemsMenuButton.css";
 
 interface IProps {
@@ -21,12 +22,12 @@ export default class extends React.Component<IProps, IState> {
 
         return (
             <div className="ItemsMenuButton-container">
-                <div
+                <CircleButton
                     className="ItemsMenuButton-icon"
                     onClick={() => this.setState({ shown: !shown })}
                 >
                     <Menu />
-                </div>
+                </CircleButton>
                 <div
                     className={"ItemsMenuButton-menu-background" + postfix}
                     onClick={() => this.setState({ shown: false })}
