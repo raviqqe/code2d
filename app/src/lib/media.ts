f05f3d2b2725c2560ceb18b84848213a5e3561bc
Yota Toyama <raviqqe@gmail.com>
2017-10-23 17:13:25 +0900

    Add touchable redux state

--- app/src/lib/media.ts
+++ app/src/lib/media.ts
@@ -1,7 +1,13 @@
-const query = matchMedia("(max-width: 768px)");
+const widthQuery = matchMedia("(max-width: 768px)");
+const touchabilityQuery = matchMedia("(pointer: coarse), (-moz-touch-enabled: 1)");
 
-export const isSmallWindow: boolean = !!query.matches;
+export const isSmallWindow: boolean = !!widthQuery.matches;
+export const touchable: boolean = !!touchabilityQuery.matches;
 
 export function onWindowSizeChange(callback: (isSmallWindow: boolean) => void): void {
-    query.addListener(({ matches }) => callback(!!matches));
+    widthQuery.addListener(({ matches }) => callback(!!matches));
+}
+
+export function onTouchabilityChange(callback: (touchable: boolean) => void): void {
+    touchabilityQuery.addListener(({ matches }) => callback(!!matches));
 }
--- app/src/redux/environment.ts
+++ app/src/redux/environment.ts
@@ -4,23 +4,25 @@ import { ImmutableObject } from "seamless-immutable";
 import actionCreatorFactory from "typescript-fsa";
 import { reducerWithInitialState } from "typescript-fsa-reducers";
 
-import { isSmallWindow, onWindowSizeChange } from "../lib/media";
+import { isSmallWindow, onTouchabilityChange, onWindowSizeChange, touchable } from "../lib/media";
 
 const actionCreator = actionCreatorFactory("ENVIRONMENT");
 
 const setIsSmallWindow = actionCreator<boolean>("SET_IS_SMALL_WINDOW");
-
-export const actionCreators = { setIsSmallWindow };
+const setTouchable = actionCreator<boolean>("SET_TOUCHABLE");
 
 export interface IState {
     isSmallWindow: boolean;
+    touchable: boolean;
 }
 
-export const initialState: ImmutableObject<IState> = Immutable({ isSmallWindow });
+export const initialState: ImmutableObject<IState> = Immutable({ isSmallWindow, touchable });
 
 export const reducer = reducerWithInitialState(initialState)
-    .case(setIsSmallWindow, (state, isSmallWindow) => state.merge({ isSmallWindow }));
+    .case(setIsSmallWindow, (state, isSmallWindow) => state.merge({ isSmallWindow }))
+    .case(setTouchable, (state, touchable) => state.merge({ touchable }));
 
 export function storeInitializer<A>(store: Store<A>): void {
     onWindowSizeChange((isSmallWindow) => store.dispatch(setIsSmallWindow(isSmallWindow)));
+    onTouchabilityChange((touchable: boolean) => store.dispatch(setTouchable(touchable)));
 }
