087bc0e116e3dced8e3249006175ced50a721814
Yota Toyama <raviqqe@gmail.com>
2017-11-04 00:07:31 +0900

    Remove stopwords file for japanese

--- functions/rakefile.rb
+++ functions/rakefile.rb
@@ -1,8 +1,5 @@
 task :deps do
   sh 'npm install'
-  # Supress warnings from node-unfluff module about a missing stopwords file
-  # for Japanese.
-  sh 'touch node_modules/unfluff/data/stopwords/stopwords-ja.txt'
 end
 
 task build: :deps do
