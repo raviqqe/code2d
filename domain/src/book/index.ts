93ba08876e38162d1f6c2d6a9293fdd03c924f92
Yota Toyama <raviqqe@gmail.com>
2017-11-04 22:13:31 +0900

    Fix language code

--- domain/src/book/index.ts
+++ domain/src/book/index.ts
@@ -1,6 +1,6 @@
 export function convertLocaleIntoBookStoreUrl(country: string, languages: string[]): string {
     // country is ISO-3166-1 country code of 2 letters.
-    return country === "JP" && languages.includes("ja")
+    return country === "JP" && languages.includes("ja-JP")
         ? "https://books.rakuten.co.jp/book/computer/"
         : "https://www.betterworldbooks.com/computer-books-technology-books-H817.aspx";
 }
