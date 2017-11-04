80a4eedbb03a6331dcf37414e75a97a2d91af1fd
Yota Toyama <raviqqe@gmail.com>
2017-11-04 23:41:17 +0900

    Fix submodule script paths

--- domain/webpack.config.js
+++ domain/webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
     extensions: [ '.ts', '.js' ]
   },
   output: {
-    filename: '[name].js',
+    filename: '[name]/index.js',
     path: path.resolve(__dirname, 'dist')
   }
 }
