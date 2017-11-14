7558638ad04fe79d0349392ee045dc1d03af57c3
Yota Toyama <raviqqe@gmail.com>
2017-11-14 22:39:19 +0900

    Add colored option to Link component

--- app/src/component/Link.tsx
+++ app/src/component/Link.tsx
@@ -3,16 +3,17 @@ import * as React from "react";
 import "./style/Link.css";
 
 interface IProps {
+    colored?: boolean;
     href: string;
 }
 
 export default class extends React.Component<IProps> {
     public render() {
-        const { children, href } = this.props;
+        const { children, colored, href } = this.props;
 
         return (
             <a
-                className="Link-container"
+                className={"Link-container" + (colored ? "-colored" : "")}
                 href={href}
                 onClick={(event) => event.stopPropagation()}
                 target="_blank"
