7be52f39f0980a475ae5e991c698dc54042e0f98
Yota Toyama <raviqqe@gmail.com>
2017-11-15 17:12:04 +0900

    Add pointer query

--- app/src/infra/media.ts
+++ app/src/infra/media.ts
@@ -1,8 +1,10 @@
 const widthQuery = matchMedia("(max-width: 768px)");
 const touchabilityQuery = matchMedia("(pointer: coarse), (-moz-touch-enabled: 1)");
+const pointerQuery = matchMedia("(any-pointer: fine)");
 
 export const isSmallWindow: boolean = !!widthQuery.matches;
 export const touchable: boolean = !!touchabilityQuery.matches;
+export const pointerAvailable: boolean = !!pointerQuery.matches;
 
 export function onWindowSizeChange(callback: (isSmallWindow: boolean) => void): void {
     widthQuery.addListener(({ matches }) => callback(!!matches));
@@ -10,4 +12,8 @@ export function onWindowSizeChange(callback: (isSmallWindow: boolean) => void): 
 
 export function onTouchabilityChange(callback: (touchable: boolean) => void): void {
     touchabilityQuery.addListener(({ matches }) => callback(!!matches));
+}
+
+export function onPointerChange(callback: (pointerAvailable: boolean) => void): void {
+    pointerQuery.addListener(({ matches }) => callback(!!matches));
 }
