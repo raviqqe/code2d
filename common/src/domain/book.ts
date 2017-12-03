4cb567e270d1bddea29cfc9572bc0c4f5e340135
Yota Toyama <raviqqe@gmail.com>
2017-12-03 16:48:57 +0900

    Update betterworldbooks URL for computer and technology page

--- common/src/domain/book.ts
+++ common/src/domain/book.ts
@@ -21,5 +21,6 @@ export function convertCountryIntoBookStoreUrl(country: string): string {
     // country is ISO-3166-1 country code of 2 letters.
     return country === "JP"
         ? "https://books.rakuten.co.jp/book/computer/"
-        : "https://www.betterworldbooks.com/computer-books-technology-books-H817.aspx";
+        : ("https://www.betterworldbooks.com/search/results" +
+            "?q=&Categories=Computer%20Books%20%26%20Technology");
 }
